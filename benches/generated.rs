d409a62cbbd0e3ba1072beb5ce24216560b24e30
Emil Sjölander <emil@visly.app>
2018-12-05 15:58:30 +0000

    Fix free space calculation for flexing

--- benches/generated.rs
+++ benches/generated.rs
@@ -5,21 +5,23 @@ extern crate stretch;
 use criterion::Criterion;
 
 fn stretch_benchmarks(c: &mut Criterion) {
-    c.bench_function("layout", |b| b.iter(|| stretch::compute(
-        &stretch::style::Node {
-            align_items: stretch::style::AlignItems::Center,
-            width: stretch::style::Dimension::Points(100.0000),
-            height: stretch::style::Dimension::Points(100.0000),
+    c.bench_function("layout", |b| {
+        b.iter(|| {
+            stretch::compute(&stretch::style::Node {
+                align_items: stretch::style::AlignItems::Center,
+                width: stretch::style::Dimension::Points(100.0000),
+                height: stretch::style::Dimension::Points(100.0000),
 
-            children: vec![stretch::style::Node {
-                width: stretch::style::Dimension::Points(10.0000),
-                height: stretch::style::Dimension::Points(10.0000),
+                children: vec![stretch::style::Node {
+                    width: stretch::style::Dimension::Points(10.0000),
+                    height: stretch::style::Dimension::Points(10.0000),
+                    ..Default::default()
+                }],
+
                 ..Default::default()
-            }],
-            
-            ..Default::default()
-        }))
-    );
+            })
+        })
+    });
 }
 
 criterion_group!(benches, stretch_benchmarks);
