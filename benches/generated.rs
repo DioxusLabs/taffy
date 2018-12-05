079ae0fc3af6e694a8088e8b86a6d7288acaaa85
Emil Sjölander <emil@visly.app>
2018-12-05 15:46:03 +0000

    Basic benchmarking setup. These should be auto-generated later like tests

---
+++ benches/generated.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate criterion;
+extern crate stretch;
+
+use criterion::Criterion;
+
+fn stretch_benchmarks(c: &mut Criterion) {
+    c.bench_function("layout", |b| b.iter(|| stretch::compute(
+        &stretch::style::Node {
+            align_items: stretch::style::AlignItems::Center,
+            width: stretch::style::Dimension::Points(100.0000),
+            height: stretch::style::Dimension::Points(100.0000),
+
+            children: vec![stretch::style::Node {
+                width: stretch::style::Dimension::Points(10.0000),
+                height: stretch::style::Dimension::Points(10.0000),
+                ..Default::default()
+            }],
+            
+            ..Default::default()
+        }))
+    );
+}
+
+criterion_group!(benches, stretch_benchmarks);
+criterion_main!(benches);
