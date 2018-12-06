e190f99006621cf58b63f8cd20d4957941157d4b
Emil Sjölander <emil@visly.app>
2018-12-06 19:09:58 +0000

    fix flex hypothetical_inner_main_size calc

--- src/algo.rs
+++ src/algo.rs
@@ -368,7 +368,8 @@ fn compute_internal(
                 percent_calc_base_child,
             ).size
             .width
-            .min(child.node.width.resolve(percent_calc_base_child, f32::MAX))
+            .min(child.node.width.resolve(percent_calc_base_child, f32::NAN))
+            .max(child.node.min_width.resolve(percent_calc_base_child, f32::MIN))
         } else {
             child.node.min_main_size(node.flex_direction).resolve(percent_calc_base_child, f32::MIN)
         };
