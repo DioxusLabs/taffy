059be099f10c50035a15cb5220f6de70f2027e6f
Emil Sjölander <emil@visly.app>
2019-01-06 22:09:17 -0800

    cargo fmt

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,17 +3,11 @@ use stretch::style::*;
 
 fn main() {
     let node = Node {
-        size: Size {
-            width: Dimension::Points(100.0),
-            height: Dimension::Points(100.0),
-        },
+        size: Size { width: Dimension::Points(100.0), height: Dimension::Points(100.0) },
         justify_content: JustifyContent::Center,
 
         children: vec![Node {
-            size: Size {
-                width: Dimension::Percent(0.5),
-                height: Dimension::Auto,
-            },
+            size: Size { width: Dimension::Percent(0.5), height: Dimension::Auto },
             ..Default::default()
         }],
 
