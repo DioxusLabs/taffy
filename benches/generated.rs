e9216b6054e7b14c13a5d166586b57f71b53cec8
Emil Sjölander <emil@visly.app>
2019-01-09 12:58:52 -0800

    Make benchmark much more complicated

--- benches/generated.rs
+++ benches/generated.rs
@@ -1,23 +1,99 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn stretch_benchmarks(c: &mut Criterion) {
-    c.bench_function("layout", |b| {
+    c.bench_function("deep hierarchy", |b| {
         b.iter(|| {
             stretch::compute(
                 &stretch::style::Node {
-                    align_items: stretch::style::AlignItems::Center,
-                    size: stretch::geometry::Size {
-                        width: stretch::style::Dimension::Points(100.0000),
-                        height: stretch::style::Dimension::Points(100.0000),
-                    },
+                    children: vec![
+                        stretch::style::Node {
+                            children: vec![
+                                stretch::style::Node {
+                                    children: vec![
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                    ],
+                                    ..Default::default()
+                                },
+                                stretch::style::Node {
+                                    children: vec![
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                    ],
+                                    ..Default::default()
+                                },
+                            ],
 
-                    children: vec![stretch::style::Node {
-                        size: stretch::geometry::Size {
-                            width: stretch::style::Dimension::Points(10.0000),
-                            height: stretch::style::Dimension::Points(10.0000),
+                            ..Default::default()
                         },
-                        ..Default::default()
-                    }],
+                        stretch::style::Node {
+                            children: vec![
+                                stretch::style::Node {
+                                    children: vec![
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                    ],
+                                    ..Default::default()
+                                },
+                                stretch::style::Node {
+                                    children: vec![
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                        stretch::style::Node {
+                                            size: stretch::geometry::Size {
+                                                width: stretch::style::Dimension::Points(10.0),
+                                                height: stretch::style::Dimension::Points(10.0),
+                                            },
+                                            ..Default::default()
+                                        },
+                                    ],
+                                    ..Default::default()
+                                },
+                            ],
+                            ..Default::default()
+                        },
+                    ],
 
                     ..Default::default()
                 },
