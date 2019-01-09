984a7c14b24cd871514fb59ee272bd9d08f787b4
Emil Sjölander <emil@visly.app>
2019-01-09 12:31:38 -0800

    fix benchmarks

--- benches/generated.rs
+++ benches/generated.rs
@@ -3,23 +3,26 @@ use criterion::{criterion_group, criterion_main, Criterion};
 fn stretch_benchmarks(c: &mut Criterion) {
     c.bench_function("layout", |b| {
         b.iter(|| {
-            stretch::compute(&stretch::style::Node {
-                align_items: stretch::style::AlignItems::Center,
-                size: stretch::geometry::Size {
-                    width: stretch::style::Dimension::Points(100.0000),
-                    height: stretch::style::Dimension::Points(100.0000),
-                },
-
-                children: vec![stretch::style::Node {
+            stretch::compute(
+                &stretch::style::Node {
+                    align_items: stretch::style::AlignItems::Center,
                     size: stretch::geometry::Size {
-                        width: stretch::style::Dimension::Points(10.0000),
-                        height: stretch::style::Dimension::Points(10.0000),
+                        width: stretch::style::Dimension::Points(100.0000),
+                        height: stretch::style::Dimension::Points(100.0000),
                     },
-                    ..Default::default()
-                }],
 
-                ..Default::default()
-            })
+                    children: vec![stretch::style::Node {
+                        size: stretch::geometry::Size {
+                            width: stretch::style::Dimension::Points(10.0000),
+                            height: stretch::style::Dimension::Points(10.0000),
+                        },
+                        ..Default::default()
+                    }],
+
+                    ..Default::default()
+                },
+                stretch::geometry::Size::undefined(),
+            )
         })
     });
 }
