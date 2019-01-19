76e5fdc2493312f8e513fc44a101499e65f0e378
Emil Sjölander <emil@visly.app>
2019-01-19 08:15:48 +0000

    Fix clippy warning

--- src/ref_eq.rs
+++ src/ref_eq.rs
@@ -1,5 +1,5 @@
 /// Determine if two borrowed pointers point to the same thing.
 #[inline]
-pub fn ref_eq<'a, 'b, T>(thing: &'a T, other: &'b T) -> bool {
+pub fn ref_eq<T>(thing: &T, other: &T) -> bool {
     (thing as *const T) == (other as *const T)
 }
