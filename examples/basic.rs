ca57fd61c28e2e87f9951df6e9a5e323d528c1e3
Emil Sjölander <emil@visly.app>
2019-04-05 13:09:07 +0100

    update sample to use new api

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,20 +1,21 @@
 use stretch::geometry::Size;
+use stretch::node::Node;
 use stretch::style::*;
 
 fn main() {
-    let node = Node {
-        size: Size { width: Dimension::Points(100.0), height: Dimension::Points(100.0) },
-        justify_content: JustifyContent::Center,
-
-        children: vec![Node {
-            size: Size { width: Dimension::Percent(0.5), height: Dimension::Auto },
+    let node = Node::new(
+        Style {
+            size: Size { width: Dimension::Points(100.0), height: Dimension::Points(100.0) },
+            justify_content: JustifyContent::Center,
             ..Default::default()
-        }],
+        },
+        vec![&Node::new(
+            Style { size: Size { width: Dimension::Percent(0.5), height: Dimension::Auto }, ..Default::default() },
+            vec![],
+        )],
+    );
 
-        ..Default::default()
-    };
-
-    let layout = stretch::compute(&node, Size::undefined()).unwrap();
+    let layout = node.compute_layout(Size::undefined()).unwrap();
 
     println!("{:#?}", layout);
 }
