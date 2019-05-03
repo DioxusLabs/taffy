249877c333f757947d1b588e9d24357a923a1ff7
msiglreith <m.siglreith@gmail.com>
2019-05-03 17:42:00 +0200

    Prepare algo for transition towards index based API

--- src/result.rs
+++ src/result.rs
@@ -9,12 +9,17 @@ use crate::number::Number;
 
 pub type Result<T> = core::result::Result<T, Box<Any>>;
 
-#[derive(Debug, Clone)]
+#[derive(Copy, Debug, Clone)]
 pub struct Layout {
     pub(crate) order: u32,
     pub size: Size<f32>,
     pub location: Point<f32>,
-    pub children: Vec<Layout>,
+}
+
+impl Layout {
+    pub(crate) fn new() -> Self {
+        Layout { order: 0, size: Size { width: 0.0, height: 0.0 }, location: Point { x: 0.0, y: 0.0 } }
+    }
 }
 
 #[derive(Debug, Clone)]
