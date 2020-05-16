dfdb5a21fc84b657c51b185abe30c54c19fb89b6
Adam Nemecek <adamnemecek@gmail.com>
2020-05-16 08:11:43 -0700

    added prelude + some refactoring

---
+++ src/prelude.rs
@@ -0,0 +1,36 @@
+pub use crate::geometry::{
+    Rect,
+    Size
+};
+
+pub use crate::node::{
+    Node,
+    Stretch
+};
+
+pub use crate::number::{
+    Number
+};
+
+pub use crate::result::{
+    Layout
+};
+
+pub use crate::style::{
+    AlignContent,
+    AlignItems,
+    AlignSelf,
+    Dimension,
+    Direction,
+    Display,
+    FlexDirection,
+    FlexWrap,
+    JustifyContent,
+    Overflow,
+    PositionType,
+    Style,
+};
+
+pub use crate::{
+    Error
+};
