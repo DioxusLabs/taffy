addeb00ba862ddf0e4ba6a4a9cba1a90b08ed7ec
François <mockersf@gmail.com>
2022-05-10 23:36:23 +0200

    remove round_layout changes

--- src/result.rs
+++ src/result.rs
@@ -8,12 +8,11 @@ pub struct Layout {
     pub(crate) order: u32,
     pub size: Size<f32>,
     pub location: Point<f32>,
-    pub(crate) relative_location: Point<f32>,
 }
 
 impl Layout {
     pub(crate) fn new() -> Self {
-        Self { order: 0, size: Size::zero(), location: Point::zero(), relative_location: Point::zero() }
+        Self { order: 0, size: Size::zero(), location: Point::zero() }
     }
 }
 
