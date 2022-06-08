851a7fd9766d71a54cbf5014ab6ddfc9e2820883
Alice Cecile <alice.i.cecile@gmail.com>
2022-06-08 18:14:29 -0400

    Document layout.rs

--- src/layout.rs
+++ src/layout.rs
@@ -1,3 +1,5 @@
+//! Final and cached data structures that represent the high-level UI layout
+
 use crate::flexbox::ComputeResult;
 use crate::geometry::{Point, Size};
 use crate::number::Number;
@@ -10,23 +12,29 @@ pub struct Layout {
     /// Nodes with a higher order should be rendered on top of those with a lower order.
     /// This is effectively a topological sort of each tree.
     pub order: u32,
-    // The width and height of the node
+    /// The width and height of the node
     pub size: Size<f32>,
-    // The bottom-left corner of the node
+    /// The bottom-left corner of the node
     pub location: Point<f32>,
 }
 
 impl Layout {
+    /// Creates a new [`Layout`] struct with zero size positioned at the origin
     pub(crate) fn new() -> Self {
         Self { order: 0, size: Size::zero(), location: Point::zero() }
     }
 }
 
+/// Cached intermediate layout results
 #[derive(Debug, Clone)]
 pub(crate) struct Cache {
+    /// The initial cached size of the node itself
     pub(crate) node_size: Size<Number>,
+    /// The initial cached size of the parent's node
     pub(crate) parent_size: Size<Number>,
+    /// Whether or not layout should be recomputed
     pub(crate) perform_layout: bool,
 
+    /// The stored result of the layout calculations
     pub(crate) result: ComputeResult,
 }
