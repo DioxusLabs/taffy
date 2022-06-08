6dfc13b44b451cb611644a291b2531a170f9e604
Alice Cecile <alice.i.cecile@gmail.com>
2022-06-08 17:54:31 -0400

    Document geometry.rs

--- src/geometry.rs
+++ src/geometry.rs
@@ -5,23 +5,37 @@ use core::ops::Add;
 use crate::number::Number;
 use crate::style::{Dimension, FlexDirection};
 
+/// The spatial extent of an axis-aligned UI rectangle
 #[derive(Debug, Copy, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(default))]
 pub struct Rect<T> {
+    /// The x-coordinate of the left edge
     pub start: T,
+    /// The x-coordinate of the right edge
     pub end: T,
+    /// The y-coordinate of the top edge
     pub top: T,
+    /// the y-coordinate of the bottom edge
     pub bottom: T,
 }
 
 impl<T> Rect<T> {
+    /// Applies the function `f` to all four sides of the [`Rect`]
+    ///
+    /// This is used to transform a `Rect<T>` into a `Rect<R>`.
     pub(crate) fn map<R, F>(self, f: F) -> Rect<R>
     where
         F: Fn(T) -> R,
     {
         Rect { start: f(self.start), end: f(self.end), top: f(self.top), bottom: f(self.bottom) }
     }
+
+    /// Applies the function `f` to all four sides of the rect
+    ///
+    /// When applied to the left and right sides, the width is used
+    /// as the second parameter of `f`.
+    /// When applied to the top or bottom sides, the height is used instead.
     pub(crate) fn zip_size<R, F, U>(self, size: Size<U>, f: F) -> Rect<R>
     where
         F: Fn(T, U) -> R,
@@ -40,14 +54,26 @@ impl<T> Rect<T>
 where
     T: Add<Output = T> + Copy + Clone,
 {
+    /// The sum of the coordinates of the left and right side of the rectangle
+    ///
+    /// **NOTE:** this is *not* the width of the rectangle.
     pub(crate) fn horizontal(&self) -> T {
         self.start + self.end
     }
 
+    /// The sum of the coordinates of the bottom and top side of the rectangle
+    ///
+    /// **NOTE:** this is *not* the height of the rectangle.
     pub(crate) fn vertical(&self) -> T {
         self.top + self.bottom
     }
 
+    /// The main layout-axis sum of the rectangle
+    ///
+    /// This is always perpendicular to [`Rect::cross`].
+    ///
+    /// If the [`FlexDirection`] is [`FlexDirection::Row`] or [`FlexDirection::RowReverse`], this is [`Rect::horizontal`].
+    /// Otherwise, this is [`Rect::vertical`].
     pub(crate) fn main(&self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.horizontal()
@@ -56,6 +82,12 @@ where
         }
     }
 
+    /// The cross layout-axis sum of the rectangle
+    ///
+    /// This is always perpendicular to [`Rect::main`].
+    ///
+    /// If the [`FlexDirection`] is [`FlexDirection::Row`] or [`FlexDirection::RowReverse`], this is [`Rect::vertical`].
+    /// Otherwise, this is [`Rect::horizontal`].
     pub(crate) fn cross(&self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.vertical()
@@ -69,6 +101,7 @@ impl<T> Rect<T>
 where
     T: Copy + Clone,
 {
+    /// The lowest coordinate of the rectangle, from the perspective of the main layout axis
     pub(crate) fn main_start(&self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.start
@@ -77,6 +110,7 @@ where
         }
     }
 
+    /// The highest coordinate of the rectangle, from the perspective of the main layout axis
     pub(crate) fn main_end(&self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.end
@@ -85,6 +119,7 @@ where
         }
     }
 
+    /// The lowest coordinate of the rectangle, from the perspective of the cross layout axis
     pub(crate) fn cross_start(&self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.top
@@ -93,6 +128,7 @@ where
         }
     }
 
+    /// The highest coordinate of the rectangle, from the perspective of the cross layout axis
     pub(crate) fn cross_end(&self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.bottom
@@ -102,21 +138,28 @@ where
     }
 }
 
+/// The width and height of a [`Rect`]
 #[derive(Debug, Copy, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "serde", serde(default))]
 pub struct Size<T> {
+    /// The x extent of the rectangle
     pub width: T,
+    /// The y extent of the rectangle
     pub height: T,
 }
 
 impl Size<()> {
+    /// Generates a `Size<Number>` with undefined width and height
     pub fn undefined() -> Size<Number> {
         Size { width: Number::Undefined, height: Number::Undefined }
     }
 }
 
 impl<T> Size<T> {
+    /// Applies the function `f` to both the width and height
+    ///
+    /// This is used to transform a `Rect<T>` into a `Rect<R>`.
     pub fn map<R, F>(self, f: F) -> Size<R>
     where
         F: Fn(T) -> R,
@@ -124,6 +167,9 @@ impl<T> Size<T> {
         Size { width: f(self.width), height: f(self.height) }
     }
 
+    /// Sets the extent of the main layout axis
+    ///
+    /// Whether this is the width or height depends on the `direction` provided
     pub(crate) fn set_main(&mut self, direction: FlexDirection, value: T) {
         if direction.is_row() {
             self.width = value
@@ -132,6 +178,9 @@ impl<T> Size<T> {
         }
     }
 
+    /// Sets the extent of the cross layout axis
+    ///
+    /// Whether this is the width or height depends on the `direction` provided
     pub(crate) fn set_cross(&mut self, direction: FlexDirection, value: T) {
         if direction.is_row() {
             self.height = value
@@ -140,6 +189,9 @@ impl<T> Size<T> {
         }
     }
 
+    /// Gets the extent of the main layout axis
+    ///
+    /// Whether this is the width or height depends on the `direction` provided
     pub(crate) fn main(self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.width
@@ -148,6 +200,9 @@ impl<T> Size<T> {
         }
     }
 
+    /// Gets the extent of the cross layout axis
+    ///
+    /// Whether this is the width or height depends on the `direction` provided
     pub(crate) fn cross(self, direction: FlexDirection) -> T {
         if direction.is_row() {
             self.height
@@ -158,6 +213,7 @@ impl<T> Size<T> {
 }
 
 impl Size<f32> {
+    /// A [`Size`] with zero width and height
     pub fn zero() -> Self {
         Self { width: 0.0, height: 0.0 }
     }
@@ -169,13 +225,19 @@ impl Size<Dimension> {
     }
 }
 
+/// A 2-dimensional coordinate.
+///
+/// When used in association with a [`Rect`], represents the bottom-left corner.
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Point<T> {
+    /// The x-coordinate
     pub x: T,
+    /// The y-coordinate
     pub y: T,
 }
 
 impl Point<f32> {
+    /// A [`Point`] with values (0,0), representing the origin
     pub fn zero() -> Self {
         Self { x: 0.0, y: 0.0 }
     }
