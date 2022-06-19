00cfab59e84fd836e07329b08b9c775a285d1a79
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2022-06-19 17:26:15 +0200

    attempting pushing to vec

--- src/node.rs
+++ src/node.rs
@@ -650,7 +650,10 @@ mod tests {
         let child1 = taffy.new_with_children(FlexboxLayout::default(), &[]).unwrap();
         let node = taffy.new_with_children(FlexboxLayout::default(), &[child0, child1]).unwrap();
 
-        let children: sys::Vec<Node> = sys::Vec::from([child0, child1]);
+        let mut children: sys::Vec<Node> = sys::Vec::new();
+        children.push(child0);
+        children.push(child1);
+
         let children_result = taffy.children(node).unwrap();
         assert_eq!(children_result, children);
 
