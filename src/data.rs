dbfcd63b9987d141fcf9b0119f00a421a42fbd2a
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-07-15 21:10:38 -0400

    feat: move measurefunc out of nodedata

--- src/data.rs
+++ src/data.rs
@@ -12,8 +12,7 @@ use crate::style::FlexboxLayout;
 pub(crate) struct NodeData {
     /// The layout strategy used by this node
     pub(crate) style: FlexboxLayout,
-    /// The mapping from the Size<Option<f32>> (in real units) to Size<f32> (in points) for this node
-    pub(crate) measure: Option<MeasureFunc>,
+
     /// The results of the layout computation
     pub(crate) layout: Layout,
     /// The primary cached results of the layout computation
@@ -25,30 +24,10 @@ pub(crate) struct NodeData {
 }
 
 impl NodeData {
-    /// Create the data for a new node with a [`MeasureFunc`]
-    #[must_use]
-    pub fn new_with_measure(style: FlexboxLayout, measure: MeasureFunc) -> Self {
-        Self {
-            style,
-            measure: Some(measure),
-            main_size_layout_cache: None,
-            other_layout_cache: None,
-            layout: Layout::new(),
-            is_dirty: true,
-        }
-    }
-
     /// Create the data for a new node
     #[must_use]
     pub fn new(style: FlexboxLayout) -> Self {
-        Self {
-            style,
-            measure: None,
-            main_size_layout_cache: None,
-            other_layout_cache: None,
-            layout: Layout::new(),
-            is_dirty: true,
-        }
+        Self { style, main_size_layout_cache: None, other_layout_cache: None, layout: Layout::new(), is_dirty: true }
     }
 
     /// Marks a node and all of its parents (recursively) as dirty
