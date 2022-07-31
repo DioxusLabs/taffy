8eb56a7fb2e337f09960444eca5ebd32a49551df
Nico Burns <nico@nicoburns.com>
2022-07-31 01:53:28 +0100

    Add support for generating Taffy Grid styles in tests to gentest main.rs

--- scripts/gentest/src/main.rs
+++ scripts/gentest/src/main.rs
@@ -2,11 +2,12 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
+use convert_case::{Case, Casing};
 use fantoccini::{Client, ClientBuilder};
 
 use log::*;
 use proc_macro2::{Span, TokenStream};
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::Ident;
 
 #[tokio::main]
@@ -203,13 +204,88 @@ fn generate_assertions(ident: &str, node: &json::JsonValue) -> TokenStream {
     )
 }
 
+// type QuoteProp = Fn(prop_name: &str, quoter: impl Fn(&json::object::Object) -> TokenStream) -> TokenStream;
+
 fn generate_node(ident: &str, node: &json::JsonValue) -> TokenStream {
     let style = &node["style"];
+
+    fn quote_object_prop(
+        prop_name: &str,
+        style: &json::JsonValue,
+        quoter: impl Fn(&json::object::Object) -> TokenStream,
+    ) -> TokenStream {
+        let prop_name_snake_case = prop_name.to_case(Case::Snake);
+        let prop_name_camel_case = prop_name.to_case(Case::Camel);
+        let prop_name_ident = format_ident!("{}", prop_name_snake_case);
+        match style[prop_name_camel_case.clone()] {
+            json::JsonValue::Object(ref value) => {
+                let prop_value = quoter(value);
+                quote!(#prop_name_ident: #prop_value,)
+            }
+            _ => quote!(),
+        }
+    }
+
+    fn quote_array_prop(
+        prop_name: &str,
+        style: &json::JsonValue,
+        quoter: impl Fn(&Vec<json::JsonValue>) -> TokenStream,
+    ) -> TokenStream {
+        let prop_name_snake_case = prop_name.to_case(Case::Snake);
+        let prop_name_camel_case = prop_name.to_case(Case::Camel);
+        let prop_name_ident = format_ident!("{}", prop_name_snake_case);
+        match style[prop_name_camel_case.clone()] {
+            json::JsonValue::Array(ref value) => {
+                let prop_value = quoter(value);
+                quote!(#prop_name_ident: #prop_value,)
+            }
+            _ => quote!(),
+        }
+    }
+
+    fn quote_string_prop(
+        prop_name: &str,
+        style: &json::JsonValue,
+        quoter: impl Fn(&str) -> TokenStream,
+    ) -> TokenStream {
+        let prop_name_snake_case = prop_name.to_case(Case::Snake);
+        let prop_name_camel_case = prop_name.to_case(Case::Camel);
+        let prop_name_ident = format_ident!("{}", prop_name_snake_case);
+        match style[prop_name_camel_case.clone()] {
+            json::JsonValue::Short(ref value) => {
+                let prop_value = quoter(value.as_ref());
+                quote!(#prop_name_ident: #prop_value,)
+            }
+            json::JsonValue::String(ref value) => {
+                let prop_value = quoter(value.as_ref());
+                quote!(#prop_name_ident: #prop_value,)
+            }
+            _ => quote!(),
+        }
+    }
+
+    fn quote_number_prop<T: From<json::number::Number>>(
+        prop_name: &str,
+        style: &json::JsonValue,
+        quoter: impl Fn(T) -> TokenStream,
+    ) -> TokenStream {
+        let prop_name_snake_case = prop_name.to_case(Case::Snake);
+        let prop_name_camel_case = prop_name.to_case(Case::Camel);
+        let prop_name_ident = format_ident!("{}", prop_name_snake_case);
+        match style[prop_name_camel_case.clone()] {
+            json::JsonValue::Number(ref value) => {
+                let prop_value = quoter(value.clone().into());
+                quote!(#prop_name_ident: #prop_value,)
+            }
+            _ => quote!(),
+        }
+    }
 
     let display = match style["display"] {
         json::JsonValue::Short(ref value) => match value.as_ref() {
             "none" => quote!(display: taffy::style::Display::None,),
-            _ => quote!(),
+            "grid" => quote!(display: taffy::style::Display::Grid,),
+            _ => quote!(display: taffy::style::Display::Flex,),
         },
         _ => quote!(),
     };
@@ -307,53 +383,26 @@ fn generate_node(ident: &str, node: &json::JsonValue) -> TokenStream {
         _ => quote!(),
     };
 
-    let flex_grow = match style["flexGrow"] {
-        json::JsonValue::Number(value) => {
-            let value: f32 = value.into();
-            quote!(flex_grow: #value,)
-        }
-        _ => quote!(),
-    };
+    let flex_grow = quote_number_prop("flex_grow", style, |value: f32| quote!(#value));
+    let flex_shrink = quote_number_prop("flex_shrink", style, |value: f32| quote!(#value));
 
-    let flex_shrink = match style["flexShrink"] {
-        json::JsonValue::Number(value) => {
-            let value: f32 = value.into();
-            quote!(flex_shrink: #value,)
-        }
-        _ => quote!(),
-    };
+    let flex_basis = quote_object_prop("flex_basis", style, generate_dimension);
+    let size = quote_object_prop("size", style, generate_size);
+    let min_size = quote_object_prop("min_size", style, generate_size);
+    let max_size = quote_object_prop("max_size", style, generate_size);
 
-    let flex_basis = match style["flexBasis"] {
-        json::JsonValue::Object(ref value) => {
-            let value = generate_dimension(value);
-            quote!(flex_basis: #value,)
-        }
-        _ => quote!(),
-    };
+    let gap = quote_object_prop("gap", style, generate_gap);
 
-    let size = match style["size"] {
-        json::JsonValue::Object(ref value) => {
-            let size = generate_size(value);
-            quote!(size: #size,)
-        }
-        _ => quote!(),
-    };
+    let grid_template_rows = quote_array_prop("grid_template_rows", style, generate_track_definition_list);
+    let grid_template_columns = quote_array_prop("grid_template_columns", style, generate_track_definition_list);
+    let grid_auto_rows = quote_array_prop("grid_auto_rows", style, generate_track_definition_list);
+    let grid_auto_columns = quote_array_prop("grid_auto_columns", style, generate_track_definition_list);
+    let grid_auto_flow = quote_object_prop("grid_auto_flow", style, generate_grid_auto_flow);
 
-    let min_size = match style["minSize"] {
-        json::JsonValue::Object(ref value) => {
-            let min_size = generate_size(value);
-            quote!(min_size: #min_size,)
-        }
-        _ => quote!(),
-    };
-
-    let max_size = match style["maxSize"] {
-        json::JsonValue::Object(ref value) => {
-            let max_size = generate_size(value);
-            quote!(max_size: #max_size,)
-        }
-        _ => quote!(),
-    };
+    let grid_row_start = quote_object_prop("grid_row_start", style, generate_grid_position);
+    let grid_row_end = quote_object_prop("grid_row_end", style, generate_grid_position);
+    let grid_column_start = quote_object_prop("grid_column_start", style, generate_grid_position);
+    let grid_column_end = quote_object_prop("grid_column_end", style, generate_grid_position);
 
     macro_rules! edges_quoted {
         ($style:ident, $val:ident) => {
@@ -412,6 +461,16 @@ fn generate_node(ident: &str, node: &json::JsonValue) -> TokenStream {
             #flex_grow
             #flex_shrink
             #flex_basis
+            #gap
+            #grid_template_rows
+            #grid_template_columns
+            #grid_auto_rows
+            #grid_auto_columns
+            #grid_auto_flow
+            #grid_row_start
+            #grid_row_end
+            #grid_column_start
+            #grid_column_end
             #size
             #min_size
             #max_size
@@ -426,21 +485,38 @@ fn generate_node(ident: &str, node: &json::JsonValue) -> TokenStream {
     ).unwrap();)
 }
 
-macro_rules! dim_quoted {
-    ($obj:ident, $dim_name:ident) => {
-        let $dim_name = match $obj.get(stringify!($dim_name)) {
+macro_rules! dim_quoted_renamed {
+    ($obj:ident, $in_name:ident, $out_name:ident) => {
+        let $out_name = match $obj.get(stringify!($in_name)) {
             Some(json::JsonValue::Object(ref value)) => {
                 let dim = generate_dimension(value);
-                quote!($dim_name: #dim,)
+                quote!($out_name: #dim,)
             }
             _ => quote!(),
         };
     };
 }
 
+macro_rules! dim_quoted {
+    ($obj:ident, $dim_name:ident) => {
+        dim_quoted_renamed!($obj, $dim_name, $dim_name)
+    };
+}
+
 fn generate_size(size: &json::object::Object) -> TokenStream {
     dim_quoted!(size, width);
     dim_quoted!(size, height);
+    quote!(
+        taffy::geometry::Size {
+            #width #height
+            ..Default::default()
+        }
+    )
+}
+
+fn generate_gap(size: &json::object::Object) -> TokenStream {
+    dim_quoted_renamed!(size, column, width);
+    dim_quoted_renamed!(size, row, height);
     quote!(
         taffy::geometry::Size {
             #width #height
@@ -480,4 +556,96 @@ fn generate_edges(dimen: &json::object::Object) -> TokenStream {
         #start #end #top #bottom
         ..Default::default()
     })
+}
+
+fn generate_grid_auto_flow(auto_flow: &json::object::Object) -> TokenStream {
+    let direction = auto_flow.get("direction").unwrap().as_str().unwrap();
+    let algorithm = auto_flow.get("algorithm").unwrap().as_str().unwrap();
+
+    match (direction, algorithm) {
+        ("row", "sparse") => quote!(taffy::style::GridAutoFlow::Row),
+        ("column", "sparse") => quote!(taffy::style::GridAutoFlow::Column),
+        ("row", "dense") => quote!(taffy::style::GridAutoFlow::RowDense),
+        ("column", "dense") => quote!(taffy::style::GridAutoFlow::ColumnDense),
+        _ => unreachable!(),
+    }
+}
+
+fn generate_grid_position(grid_position: &json::object::Object) -> TokenStream {
+    let kind = grid_position.get("kind").unwrap();
+    let value = || grid_position.get("value").unwrap().as_f32().unwrap();
+
+    match kind {
+        json::JsonValue::Short(ref kind) => match kind.as_ref() {
+            "auto" => quote!(taffy::style::GridLine::Auto),
+            "span" => {
+                let value = value() as u16;
+                quote!(taffy::style::GridLine::Span(#value))
+            }
+            "track" => {
+                let value = value() as i16;
+                quote!(taffy::style::GridLine::Track(#value))
+            }
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
+    }
+}
+
+fn generate_track_definition_list(raw_list: &Vec<json::JsonValue>) -> TokenStream {
+    let list = raw_list.into_iter().map(|obj| match obj {
+        json::JsonValue::Object(inner) => generate_track_definition(inner),
+        _ => unreachable!(),
+    });
+
+    quote!(vec![#(#list),*])
+}
+
+fn generate_track_definition(track_definition: &json::object::Object) -> TokenStream {
+    let kind = track_definition.get("kind").unwrap().as_str().unwrap();
+    let name = || track_definition.get("name").unwrap().as_str().unwrap();
+    let arguments = || track_definition.get("arguments").unwrap();
+
+    match kind {
+        "scalar" => generate_scalar_definition(track_definition),
+        "function" => match (name(), arguments()) {
+            ("minmax", json::JsonValue::Array(arguments)) => {
+                if arguments.len() != 2 {
+                    panic!("minmax function with the wrong number of arguments");
+                }
+                let min = match arguments[0] {
+                    json::JsonValue::Object(ref arg) => generate_scalar_definition(arg),
+                    _ => unreachable!(),
+                };
+                let max = match arguments[1] {
+                    json::JsonValue::Object(ref arg) => generate_scalar_definition(arg),
+                    _ => unreachable!(),
+                };
+                quote!(taffy::style::GridTrackSizingFunction::MinMax{ min: #min, max: #max })
+            }
+            // TODO: Add support for fit-content
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
+    }
+}
+
+fn generate_scalar_definition(track_definition: &json::object::Object) -> TokenStream {
+    let unit = || track_definition.get("unit").unwrap().as_str().unwrap();
+    let value = || track_definition.get("value").unwrap().as_f32().unwrap();
+
+    match unit() {
+        "auto" => quote!(taffy::style::GridTrackSizingFunction::Auto),
+        "min-content" => quote!(taffy::style::GridTrackSizingFunction::MinContent),
+        "max-content" => quote!(taffy::style::GridTrackSizingFunction::MaxContent),
+        "points" | "percent" => {
+            let value = generate_dimension(track_definition);
+            quote!(taffy::style::GridTrackSizingFunction::Fixed(#value))
+        }
+        "fraction" => {
+            let value: f32 = value();
+            quote!(taffy::style::GridTrackSizingFunction::Flex(#value))
+        }
+        _ => unreachable!(),
+    }
 }
