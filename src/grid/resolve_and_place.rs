06a5d6fc6e03bf1c1b3b3ccfe38cd0bc99d097d1
Nico Burns <nico@nicoburns.com>
2022-08-16 02:06:33 +0100

    Implement remainder of grid auto placement

--- src/grid/resolve_and_place.rs
+++ src/grid/resolve_and_place.rs
@@ -15,10 +15,8 @@ pub(super) enum CellOccupancyState {
     Unoccupied,
     // Indicates that a grid cell is occupied by a definitely placed item
     DefinitelyPlaced,
-    // Indicates that a grid cell is occupied by an item that is definitely placed in the primary axis
-    PrimaryAxisPlaced,
-    // Indicates that a grid cell is occupied by an item that is definitely placed in the secondary axis
-    SeconaryAxisPlaced,
+    // Indicates that a grid cell is occupied by an item that was placed by the auto placement algorithm
+    AutoPlaced,
 }
 
 impl CellOccupancyState {
@@ -84,6 +82,12 @@ impl TrackCounts {
         let end = self.line_index_to_proceeding_track_index(input.end);
         start..end
     }
+
+    pub fn convert_track_index_range_to_line_range(&self, input: Range<i16>) -> Line<i16> {
+        let start = self.track_index_to_preceeding_line_index(input.start as u16);
+        let end = self.track_index_to_preceeding_line_index(input.end as u16);
+        Line { start, end }
+    }
 }
 
 pub(super) struct CellOccupancyMatrix {
@@ -100,9 +104,25 @@ impl CellOccupancyMatrix {
         Self { inner: Grid::new(rows.len(), columns.len()), rows, columns }
     }
 
-    /// Expands the grid (potentially in all 4 directions) in order to ensure
-    /// That
-    fn ensure_area_is_in_range(&mut self, row_range: Range<i16>, col_range: Range<i16>) -> bool {
+    pub fn is_area_in_range(
+        &mut self,
+        primary_axis: RowColumn,
+        primary_range: Range<i16>,
+        secondary_range: Range<i16>,
+    ) -> bool {
+        if primary_range.start < 0 || primary_range.end >= self.track_counts(primary_axis).len() as i16 {
+            return false;
+        }
+        if secondary_range.start < 0
+            || secondary_range.end >= self.track_counts(primary_axis.opposite_axis()).len() as i16
+        {
+            return false;
+        }
+        true
+    }
+
+    /// Expands the grid (potentially in all 4 directions) in order to ensure that the specified range fits within the allocated space
+    fn expand_to_fit_range(&mut self, row_range: Range<i16>, col_range: Range<i16>) {
         let old_row_count = self.rows.negative_implicit + self.rows.explicit + self.rows.positive_implicit;
         let old_col_count = self.columns.negative_implicit + self.columns.explicit + self.columns.positive_implicit;
 
@@ -113,11 +133,6 @@ impl CellOccupancyMatrix {
         let req_negative_cols = min(col_range.start, 0);
         let req_positive_cols =
             max(col_range.end - self.columns.explicit as i16 - self.columns.negative_implicit as i16, 0);
-
-        // Return early (with true to indicated "already in range") if no new rows or columns are required
-        if (req_negative_rows + req_positive_rows + req_negative_cols + req_positive_cols == 0) {
-            return true;
-        }
 
         let new_row_count = self.inner.rows() + (req_negative_rows + req_positive_rows) as usize;
         let new_col_count = self.inner.cols() + (req_negative_cols + req_positive_cols) as usize;
@@ -156,9 +171,6 @@ impl CellOccupancyMatrix {
         self.rows.positive_implicit += req_positive_rows as u16;
         self.columns.negative_implicit += req_negative_cols as u16;
         self.columns.positive_implicit += req_positive_cols as u16;
-
-        // Return false to indicate "not already in range"
-        false
     }
 
     pub fn mark_area_as(
@@ -176,10 +188,11 @@ impl CellOccupancyMatrix {
         let mut row_range = self.rows.convert_grid_line_range_to_track_index_range(row_span);
         let mut col_range = self.columns.convert_grid_line_range_to_track_index_range(column_span);
 
-        // Ensure resolve ranges fit within the allocated grid. And if they didn't already fit then re-resolve the ranges
-        // once the grid has been expanded as the resolved indexes may have changed
-        let was_in_range = self.ensure_area_is_in_range(row_range.clone(), col_range.clone());
-        if !was_in_range {
+        // Check that if the resolved ranges fit within the allocated grid. And if they don't then expand the grid to fit
+        // and then re-resolve the ranges once the grid has been expanded as the resolved indexes may have changed
+        let is_in_range = self.is_area_in_range(RowColumn::Row, row_range.clone(), col_range.clone());
+        if !is_in_range {
+            self.expand_to_fit_range(row_range.clone(), col_range.clone());
             row_range = self.rows.convert_grid_line_range_to_track_index_range(row_span);
             col_range = self.columns.convert_grid_line_range_to_track_index_range(column_span);
         }
@@ -191,7 +204,15 @@ impl CellOccupancyMatrix {
         }
     }
 
-    pub fn area_is_unoccupied(
+    pub fn lines_to_tracks(&self, axis: RowColumn, span: Line<i16>) -> Range<i16> {
+        self.track_counts(axis).convert_grid_line_range_to_track_index_range(span)
+    }
+
+    pub fn tracks_to_lines(&self, axis: RowColumn, span: Range<i16>) -> Line<i16> {
+        self.track_counts(axis).convert_track_index_range_to_line_range(span)
+    }
+
+    pub fn line_area_is_unoccupied(
         &self,
         primary_axis: RowColumn,
         primary_span: Line<i16>,
@@ -204,6 +225,20 @@ impl CellOccupancyMatrix {
 
         let row_range = self.rows.convert_grid_line_range_to_track_index_range(row_span);
         let col_range = self.columns.convert_grid_line_range_to_track_index_range(column_span);
+
+        self.track_area_is_unoccupied(RowColumn::Row, row_range, col_range)
+    }
+
+    pub fn track_area_is_unoccupied(
+        &self,
+        primary_axis: RowColumn,
+        primary_range: Range<i16>,
+        secondary_range: Range<i16>,
+    ) -> bool {
+        let (row_range, col_range) = match primary_axis {
+            RowColumn::Row => (primary_range, secondary_range),
+            RowColumn::Column => (secondary_range, primary_range),
+        };
 
         // Search for occupied cells in the specified area. Out of bounds cells are considered unoccupied.
         for x in row_range {
@@ -218,10 +253,17 @@ impl CellOccupancyMatrix {
         true
     }
 
-    fn track_counts(&self, track_type: RowColumn) -> &TrackCounts {
+    pub fn track_counts(&self, track_type: RowColumn) -> &TrackCounts {
         match track_type {
             RowColumn::Row => &self.rows,
             RowColumn::Column => &self.columns,
+        }
+    }
+
+    pub fn get(&self, track_type: RowColumn, primary_index: u16, secondary_index: u16) -> Option<&CellOccupancyState> {
+        match track_type {
+            RowColumn::Row => self.inner.get(primary_index as usize, secondary_index as usize),
+            RowColumn::Column => self.inner.get(secondary_index as usize, primary_index as usize),
         }
     }
 
@@ -349,7 +391,7 @@ pub(super) fn place_grid_items(grid: &mut CssGrid, tree: &impl LayoutTree, node:
     // 3.2 Among all the items with a definite column position (explicitly positioned items, items positioned in the previous step,
     //     and items not yet positioned but with a definite column) add columns to the beginning and end of the implicit grid as necessary
     //     to accommodate those items.
-    //        => Handled by ensure_area_is_in_range which expands the GridOccupancyMatrix as necessary
+    //        => Handled by expand_to_fit_range which expands the GridOccupancyMatrix as necessary
     //            -> Called by mark_area_as
     //            -> Called by record_grid_placement
     //
@@ -385,7 +427,7 @@ pub(super) fn place_definite_primary_axis_item(grid: &mut CssGrid, node: Node, s
         true => 1,
         false => grid
             .cell_occupancy_matrix
-            .last_of_type(flow_direction, primary_axis_placement.start as i16, CellOccupancyState::PrimaryAxisPlaced)
+            .last_of_type(flow_direction, primary_axis_placement.start as i16, CellOccupancyState::AutoPlaced)
             .unwrap_or(1),
     };
 
@@ -394,7 +436,7 @@ pub(super) fn place_definite_primary_axis_item(grid: &mut CssGrid, node: Node, s
         let secondary_axis_placement =
             style.grid_placement(flow_direction.opposite_axis()).resolve_indefinite_grid_tracks(position);
 
-        let does_fit = grid.cell_occupancy_matrix.area_is_unoccupied(
+        let does_fit = grid.cell_occupancy_matrix.line_area_is_unoccupied(
             flow_direction,
             primary_axis_placement,
             secondary_axis_placement,
@@ -407,7 +449,7 @@ pub(super) fn place_definite_primary_axis_item(grid: &mut CssGrid, node: Node, s
                 flow_direction,
                 primary_axis_placement,
                 secondary_axis_placement,
-                CellOccupancyState::PrimaryAxisPlaced,
+                CellOccupancyState::AutoPlaced,
             );
             break;
         } else {
@@ -421,11 +463,69 @@ pub(super) fn place_indefinitely_positioned_item(
     node: Node,
     style: &Style,
     auto_flow: GridAutoFlow,
-    grid_position: (u16, u16),
+    mut grid_position: (u16, u16),
 ) -> (u16, u16) {
     let flow_direction = auto_flow.flow_direction();
 
-    grid_position
+    let primary_placement_style = style.grid_placement(flow_direction);
+    let secondary_placement_style = style.grid_placement(flow_direction.opposite_axis());
+
+    let primary_span = primary_placement_style.span();
+    let secondary_span = secondary_placement_style.span();
+    let has_definite_secondary_axis_position = secondary_placement_style.is_definite();
+    let secondary_axis_length = grid.cell_occupancy_matrix.track_counts(flow_direction).len() as i16;
+
+    let track_area_is_unoccupied = |primary_range, secondary_range| {
+        grid.cell_occupancy_matrix.track_area_is_unoccupied(flow_direction, primary_range, secondary_range)
+    };
+    let tracks_to_lines = |range| grid.cell_occupancy_matrix.tracks_to_lines(flow_direction, range);
+
+    let (mut primary_idx, mut secondary_idx) = grid_position;
+    if has_definite_secondary_axis_position {
+        let definite_secondary_placement = secondary_placement_style.resolve_definite_grid_tracks();
+        secondary_idx = grid
+            .cell_occupancy_matrix
+            .lines_to_tracks(flow_direction.opposite_axis(), definite_secondary_placement)
+            .start as u16;
+    }
+    loop {
+        let primary_range = (primary_idx as i16)..((primary_idx + primary_span) as i16);
+        let secondary_range = (secondary_idx as i16)..((secondary_idx + secondary_span) as i16);
+
+        // Check if item fits in it's current position, and if so place it there
+        let secondary_out_of_bounds =
+            !has_definite_secondary_axis_position && secondary_range.end >= secondary_axis_length;
+        let place_here =
+            !secondary_out_of_bounds && track_area_is_unoccupied(primary_range.clone(), secondary_range.clone());
+        if place_here {
+            // Record item placement
+            let primary_span = tracks_to_lines(primary_range.clone());
+            let secondary_span = tracks_to_lines(secondary_range.clone());
+            let placement_type = CellOccupancyState::AutoPlaced;
+            record_grid_placement(grid, node, flow_direction, primary_span, secondary_span, placement_type);
+
+            // Update grid position cursor for next item and break loop
+            grid_position = (primary_range.start as u16, secondary_range.end as u16);
+            break;
+        }
+
+        // Else check the next position
+        if has_definite_secondary_axis_position {
+            primary_idx += 1;
+        } else if secondary_out_of_bounds {
+            primary_idx += 1;
+            secondary_idx = 0;
+        } else {
+            secondary_idx += 1;
+        }
+    }
+
+    // If using the "dense" placement algorithm then return (0, 0) to reset the grid position back to the origin ready for the next item
+    // Otherwise return the position of the current item so that the next item it placed after it.
+    match auto_flow.is_dense() {
+        true => (0, 0),
+        false => grid_position,
+    }
 }
 
 /// Record a grid item once the definite placement has been determined
@@ -433,19 +533,20 @@ pub(super) fn record_grid_placement(
     grid: &mut CssGrid,
     node: Node,
     primary_axis: RowColumn,
-    row_span: Line<i16>,
-    column_span: Line<i16>,
+    primary_span: Line<i16>,
+    secondary_span: Line<i16>,
     placement_type: CellOccupancyState,
 ) {
     // Mark area of grid as occupied
-    grid.cell_occupancy_matrix.mark_area_as(primary_axis, row_span, column_span, placement_type);
+    grid.cell_occupancy_matrix.mark_area_as(primary_axis, primary_span, secondary_span, placement_type);
 
     // Create grid item
+    let (row_span, col_span) = primary_axis.into_row_column(primary_span, secondary_span);
     grid.items.push(GridItem {
         node,
         min_content_contribution: None,
         max_content_contribution: None,
         row: row_span,
-        column: column_span,
+        column: col_span,
     });
 }
