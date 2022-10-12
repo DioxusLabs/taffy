86e89877c5471745e16bf00d8020009b2c6f0bea
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-10-12 16:44:45 +0200

    Improve documentation for calculated dimensions

--- src/style/calc_dimension.rs
+++ src/style/calc_dimension.rs
@@ -23,20 +23,31 @@ pub enum CalcDimension {
 }
 
 impl MaybeResolve<Option<f32>> for CalcDimension {
+    /// Resolve the calculations to concrete values.
+    ///
+    /// If any side resolves to [`None`], [`None`] is also returned.
     fn maybe_resolve(&self, context: Option<f32>) -> Option<f32> {
         match self {
+            // Resolve both sides and add them together
+            // If either side resolves to `None`, return `None`
             CalcDimension::Add(lhs, rhs) => lhs
                 .maybe_resolve(context)
                 .zip(rhs.maybe_resolve(context))
                 .map(|(lhs_value, rhs_value)| lhs_value + rhs_value),
 
+            // Resolve both sides and subtract them
+            // If either side resolves to `None`, return `None`
             CalcDimension::Sub(lhs, rhs) => lhs
                 .maybe_resolve(context)
                 .zip(rhs.maybe_resolve(context))
                 .map(|(lhs_value, rhs_value)| lhs_value - rhs_value),
 
+            // Resolve the left side and multiply it by the factor
+            // If the left side resolves to `None`, return `None`
             CalcDimension::Mul(lhs, factor) => lhs.maybe_resolve(context).map(|lhs_value| lhs_value * factor),
 
+            // Resolve the left side and divide it by the factor
+            // If the left side resolves to `None`, return `None`
             CalcDimension::Div(lhs, divisor) => lhs.maybe_resolve(context).map(|lhs_value| lhs_value / divisor),
         }
     }
--- src/style/dimension.rs
+++ src/style/dimension.rs
@@ -28,6 +28,28 @@ pub enum Dimension {
     /// A calculation of dimensions, similar to CSS's `calc()`.
     ///
     /// One use-case of this is to add a percentage value to a points value.
+    ///
+    /// Usually, you don't want to construct this variant yourself.
+    /// Instead use the `+`, `-`, `*` and `/` operators.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// # use taffy::style::CalcDimension;
+    /// #
+    /// let calc = Dimension::Points(10.0) + Dimension::Percent(50.0);
+    /// let expected = Dimension::Calc(
+    ///     CalcDimension::Add(
+    ///         Box::new(Dimension::Points(10.0)),
+    ///         Box::new(Dimension::Percent(50.0))
+    ///     )
+    /// );
+    /// assert_eq!(calc, expected);
+    /// ```
+    ///
+    /// The actual result is then calculated when resolving the absolute values.
+    /// See [`Dimension::maybe_resolve`].
     Calc(CalcDimension),
 }
 
@@ -47,6 +69,48 @@ impl Dimension {
 impl Add<Dimension> for Dimension {
     type Output = Dimension;
 
+    /// Add two [`Dimension`]s together.
+    ///
+    /// If the result of the calculation can already be known, it is calculated immediately.
+    /// Otherwise, variants of [`Dimension::Calc`] are constructed.
+    ///
+    /// # Examples
+    ///
+    /// When the result can already be known, it is calculated immediately:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Points(13.0) + Dimension::Points(5.5);
+    /// assert_eq!(result, Dimension::Points(18.5));
+    /// ```
+    ///
+    /// If any of the operants is [`Dimension::Undefined`], the result will be [`Dimension::Undefined`]:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Points(28.0) + Dimension::Undefined;
+    /// assert_eq!(result, Dimension::Undefined);
+    /// ```
+    ///
+    /// The behavior is analogue for [`Dimension::Auto`].
+    ///
+    /// If the result cannot be known beforehand, variants of [`Dimension::Calc`] will be constructed:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// # use taffy::style::CalcDimension;
+    /// #
+    /// let result = Dimension::Points(5.0) + Dimension::Percent(80.0);
+    /// let expected = Dimension::Calc(
+    ///     CalcDimension::Add(
+    ///         Box::new(Dimension::Points(5.0)),
+    ///         Box::new(Dimension::Percent(80.0))
+    ///     )
+    /// );
+    /// assert_eq!(result, expected);
+    /// ```
     fn add(self, rhs: Dimension) -> Self::Output {
         match (self, rhs) {
             (Dimension::Undefined, _) | (_, Dimension::Undefined) => Dimension::Undefined,
@@ -62,6 +126,48 @@ impl Add<Dimension> for Dimension {
 impl Sub<Dimension> for Dimension {
     type Output = Dimension;
 
+    /// Subtract one [`Dimension`] from another.
+    ///
+    /// If the result of the calculation can already be known, it is calculated immediately.
+    /// Otherwise, variants of [`Dimension::Calc`] are constructed.
+    ///
+    /// # Examples
+    ///
+    /// When the result can already be known, it is calculated immediately:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Points(13.0) - Dimension::Points(5.5);
+    /// assert_eq!(result, Dimension::Points(7.5));
+    /// ```
+    ///
+    /// If any of the operants is [`Dimension::Undefined`], the result will be [`Dimension::Undefined`]:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Points(28.0) - Dimension::Undefined;
+    /// assert_eq!(result, Dimension::Undefined);
+    /// ```
+    ///
+    /// The behavior is analogue for [`Dimension::Auto`].
+    ///
+    /// If the result cannot be known beforehand, variants of [`Dimension::Calc`] will be constructed:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// # use taffy::style::CalcDimension;
+    /// #
+    /// let result = Dimension::Percent(80.0) - Dimension::Points(5.0);
+    /// let expected = Dimension::Calc(
+    ///     CalcDimension::Sub(
+    ///         Box::new(Dimension::Percent(80.0)),
+    ///         Box::new(Dimension::Points(5.0))
+    ///     )
+    /// );
+    /// assert_eq!(result, expected);
+    /// ```
     fn sub(self, rhs: Dimension) -> Self::Output {
         match (self, rhs) {
             (Dimension::Undefined, _) | (_, Dimension::Undefined) => Dimension::Undefined,
@@ -77,6 +183,61 @@ impl Sub<Dimension> for Dimension {
 impl Mul<f32> for Dimension {
     type Output = Dimension;
 
+    /// Multiply a [`Dimension`] with a constant factor.
+    ///
+    /// If the result of the calculation can already be known, it is calculated immediately.
+    /// Otherwise, variants of [`Dimension::Calc`] are constructed.
+    ///
+    /// # Examples
+    ///
+    /// When the result can already be known, it is calculated immediately:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Points(2.0) * 5.0;
+    /// assert_eq!(result, Dimension::Points(10.0));
+    /// ```
+    ///
+    /// If the value is [`Dimension::Undefined`], the result will be [`Dimension::Undefined`]:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Undefined * 3.0;
+    /// assert_eq!(result, Dimension::Undefined);
+    /// ```
+    ///
+    /// The behavior is analogue for [`Dimension::Auto`].
+    ///
+    /// If the result cannot be known beforehand, variants of [`Dimension::Calc`] will be constructed:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// # use taffy::style::CalcDimension;
+    /// #
+    /// let lhs = Dimension::Calc(
+    ///     CalcDimension::Sub(
+    ///         Box::new(Dimension::Percent(80.0)),
+    ///         Box::new(Dimension::Points(5.0))
+    ///     )
+    /// );
+    /// let result = lhs * 3.0;
+    /// let expected = Dimension::Calc(
+    ///     CalcDimension::Mul(
+    ///         Box::new(
+    ///             Dimension::Calc(
+    ///                 CalcDimension::Sub(
+    ///                     Box::new(Dimension::Percent(80.0)),
+    ///                     Box::new(Dimension::Points(5.0))
+    ///                 )
+    ///             )
+    ///         ),
+    ///         3.0
+    ///     )
+    /// );
+    /// assert_eq!(result, expected);
+    /// ```
     fn mul(self, factor: f32) -> Self::Output {
         match self {
             Dimension::Undefined => Dimension::Undefined,
@@ -92,6 +253,61 @@ impl Mul<f32> for Dimension {
 impl Mul<Dimension> for f32 {
     type Output = Dimension;
 
+    /// Multiply a constant factor with a [`Dimension`].
+    ///
+    /// If the result of the calculation can already be known, it is calculated immediately.
+    /// Otherwise, variants of [`Dimension::Calc`] are constructed.
+    ///
+    /// # Examples
+    ///
+    /// When the result can already be known, it is calculated immediately:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = 5.0 * Dimension::Points(2.0);
+    /// assert_eq!(result, Dimension::Points(10.0));
+    /// ```
+    ///
+    /// If the value is [`Dimension::Undefined`], the result will be [`Dimension::Undefined`]:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = 3.0 * Dimension::Undefined;
+    /// assert_eq!(result, Dimension::Undefined);
+    /// ```
+    ///
+    /// The behavior is analogue for [`Dimension::Auto`].
+    ///
+    /// If the result cannot be known beforehand, variants of [`Dimension::Calc`] will be constructed:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// # use taffy::style::CalcDimension;
+    /// #
+    /// let lhs = Dimension::Calc(
+    ///     CalcDimension::Sub(
+    ///         Box::new(Dimension::Percent(80.0)),
+    ///         Box::new(Dimension::Points(5.0))
+    ///     )
+    /// );
+    /// let result = 3.0 * lhs;
+    /// let expected = Dimension::Calc(
+    ///     CalcDimension::Mul(
+    ///         Box::new(
+    ///             Dimension::Calc(
+    ///                 CalcDimension::Sub(
+    ///                     Box::new(Dimension::Percent(80.0)),
+    ///                     Box::new(Dimension::Points(5.0))
+    ///                 )
+    ///             )
+    ///         ),
+    ///         3.0
+    ///     )
+    /// );
+    /// assert_eq!(result, expected);
+    /// ```
     fn mul(self, rhs: Dimension) -> Self::Output {
         match rhs {
             Dimension::Undefined => Dimension::Undefined,
@@ -107,6 +323,61 @@ impl Mul<Dimension> for f32 {
 impl Div<f32> for Dimension {
     type Output = Dimension;
 
+    /// Divide a [`Dimension`] by a constant factor.
+    ///
+    /// If the result of the calculation can already be known, it is calculated immediately.
+    /// Otherwise, variants of [`Dimension::Calc`] are constructed.
+    ///
+    /// # Examples
+    ///
+    /// When the result can already be known, it is calculated immediately:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Points(10.0) / 2.0;
+    /// assert_eq!(result, Dimension::Points(5.0));
+    /// ```
+    ///
+    /// If the value is [`Dimension::Undefined`], the result will be [`Dimension::Undefined`]:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// #
+    /// let result = Dimension::Undefined / 3.0;
+    /// assert_eq!(result, Dimension::Undefined);
+    /// ```
+    ///
+    /// The behavior is analogue for [`Dimension::Auto`].
+    ///
+    /// If the result cannot be known beforehand, variants of [`Dimension::Calc`] will be constructed:
+    ///
+    /// ```
+    /// # use taffy::style::Dimension;
+    /// # use taffy::style::CalcDimension;
+    /// #
+    /// let lhs = Dimension::Calc(
+    ///     CalcDimension::Sub(
+    ///         Box::new(Dimension::Percent(80.0)),
+    ///         Box::new(Dimension::Points(5.0))
+    ///     )
+    /// );
+    /// let result = lhs / 3.0;
+    /// let expected = Dimension::Calc(
+    ///     CalcDimension::Div(
+    ///         Box::new(
+    ///             Dimension::Calc(
+    ///                 CalcDimension::Sub(
+    ///                     Box::new(Dimension::Percent(80.0)),
+    ///                     Box::new(Dimension::Points(5.0))
+    ///                 )
+    ///             )
+    ///         ),
+    ///         3.0
+    ///     )
+    /// );
+    /// assert_eq!(result, expected);
+    /// ```
     fn div(self, divisor: f32) -> Self::Output {
         match self {
             Dimension::Undefined => Dimension::Undefined,
