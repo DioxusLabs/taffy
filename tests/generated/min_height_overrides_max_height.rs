be880713e1befb86fc7884a26b8fd2eadad4af41
George Atkinson <geomyles@yahoo.co.uk>
2022-11-25 13:22:10 +0000

    Add generated tests for min_size overriding max_size

---
+++ tests/generated/min_height_overrides_max_height.rs
@@ -0,0 +1,30 @@
+#[test]
+fn min_height_overrides_max_height() {
+    let mut taffy = taffy::Taffy::new();
+    let node0 = taffy
+        .new_with_children(
+            taffy::style::Style {
+                min_size: taffy::geometry::Size {
+                    height: taffy::style::Dimension::Points(100f32),
+                    ..Default::default()
+                },
+                max_size: taffy::geometry::Size {
+                    height: taffy::style::Dimension::Points(50f32),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            &[],
+        )
+        .unwrap();
+    let node = taffy.new_with_children(taffy::style::Style { ..Default::default() }, &[node0]).unwrap();
+    taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
+    assert_eq!(taffy.layout(node).unwrap().size.width, 0f32);
+    assert_eq!(taffy.layout(node).unwrap().size.height, 100f32);
+    assert_eq!(taffy.layout(node).unwrap().location.x, 0f32);
+    assert_eq!(taffy.layout(node).unwrap().location.y, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().size.width, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().size.height, 100f32);
+    assert_eq!(taffy.layout(node0).unwrap().location.x, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().location.y, 0f32);
+}
---
+++ tests/generated/min_width_overrides_max_width.rs
@@ -0,0 +1,27 @@
+#[test]
+fn min_width_overrides_max_width() {
+    let mut taffy = taffy::Taffy::new();
+    let node0 = taffy
+        .new_with_children(
+            taffy::style::Style {
+                min_size: taffy::geometry::Size {
+                    width: taffy::style::Dimension::Points(100f32),
+                    ..Default::default()
+                },
+                max_size: taffy::geometry::Size { width: taffy::style::Dimension::Points(50f32), ..Default::default() },
+                ..Default::default()
+            },
+            &[],
+        )
+        .unwrap();
+    let node = taffy.new_with_children(taffy::style::Style { ..Default::default() }, &[node0]).unwrap();
+    taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
+    assert_eq!(taffy.layout(node).unwrap().size.width, 100f32);
+    assert_eq!(taffy.layout(node).unwrap().size.height, 0f32);
+    assert_eq!(taffy.layout(node).unwrap().location.x, 0f32);
+    assert_eq!(taffy.layout(node).unwrap().location.y, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().size.width, 100f32);
+    assert_eq!(taffy.layout(node0).unwrap().size.height, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().location.x, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().location.y, 0f32);
+}
