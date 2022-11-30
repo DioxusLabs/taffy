0cf6da00a28ff51eb864dbd12c7f7f419e7be4cd
Ickshonpe <david.curthoys@googlemail.com>
2022-11-30 16:13:44 +0000

    fmt, add fix to release notes

--- tests/border_and_padding.rs
+++ tests/border_and_padding.rs
@@ -9,20 +9,23 @@ fn arr_to_rect<T: Copy>(items: [T; 4]) -> Rect<T> {
 fn border_on_a_single_axis_doesnt_increase_size() {
     for i in 0..4 {
         let mut taffy = Taffy::new();
-        let node = taffy.new_leaf(Style {
-            border: {
-                let mut lengths = [LengthPercentage::ZERO; 4];
-                lengths[i] = LengthPercentage::Points(10.);
-                arr_to_rect(lengths)
-            },
-            ..Default::default()
-        }).unwrap();
-        
-        taffy.compute_layout(
-            node, 
-            Size { height: AvailableSpace::Definite(100.0), width: AvailableSpace::Definite(100.0) }
-        )
-        .ok();
+        let node = taffy
+            .new_leaf(Style {
+                border: {
+                    let mut lengths = [LengthPercentage::ZERO; 4];
+                    lengths[i] = LengthPercentage::Points(10.);
+                    arr_to_rect(lengths)
+                },
+                ..Default::default()
+            })
+            .unwrap();
+
+        taffy
+            .compute_layout(
+                node,
+                Size { height: AvailableSpace::Definite(100.0), width: AvailableSpace::Definite(100.0) },
+            )
+            .ok();
 
         let layout = taffy.layout(node).unwrap();
         assert_eq!(layout.size.width * layout.size.height, 0.);
@@ -33,20 +36,23 @@ fn border_on_a_single_axis_doesnt_increase_size() {
 fn padding_on_a_single_axis_doesnt_increase_size() {
     for i in 0..4 {
         let mut taffy = Taffy::new();
-        let node = taffy.new_leaf(Style {
-            border: {
-                let mut lengths = [LengthPercentage::ZERO; 4];
-                lengths[i] = LengthPercentage::Points(10.);
-                arr_to_rect(lengths)
-            },
-            ..Default::default()
-        }).unwrap();
-        
-        taffy.compute_layout(
-            node, 
-            Size { height: AvailableSpace::Definite(100.0), width: AvailableSpace::Definite(100.0) }
-        )
-        .ok();
+        let node = taffy
+            .new_leaf(Style {
+                border: {
+                    let mut lengths = [LengthPercentage::ZERO; 4];
+                    lengths[i] = LengthPercentage::Points(10.);
+                    arr_to_rect(lengths)
+                },
+                ..Default::default()
+            })
+            .unwrap();
+
+        taffy
+            .compute_layout(
+                node,
+                Size { height: AvailableSpace::Definite(100.0), width: AvailableSpace::Definite(100.0) },
+            )
+            .ok();
 
         let layout = taffy.layout(node).unwrap();
         assert_eq!(layout.size.width * layout.size.height, 0.);
@@ -58,21 +64,18 @@ fn border_and_padding_on_a_single_axis_doesnt_increase_size() {
     for i in 0..4 {
         let mut taffy = Taffy::new();
         let rect = {
-             let mut lengths = [LengthPercentage::ZERO; 4];
+            let mut lengths = [LengthPercentage::ZERO; 4];
             lengths[i] = LengthPercentage::Points(10.);
             arr_to_rect(lengths)
         };
-        let node = taffy.new_leaf(Style {
-            border: rect,
-            padding: rect,
-            ..Default::default()
-        }).unwrap();
-        
-        taffy.compute_layout(
-            node, 
-            Size { height: AvailableSpace::Definite(100.0), width: AvailableSpace::Definite(100.0) }
-        )
-        .ok();
+        let node = taffy.new_leaf(Style { border: rect, padding: rect, ..Default::default() }).unwrap();
+
+        taffy
+            .compute_layout(
+                node,
+                Size { height: AvailableSpace::Definite(100.0), width: AvailableSpace::Definite(100.0) },
+            )
+            .ok();
         let layout = taffy.layout(node).unwrap();
         assert_eq!(layout.size.width * layout.size.height, 0.);
     }
