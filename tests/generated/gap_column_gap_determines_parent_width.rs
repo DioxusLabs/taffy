7f1dadb95f6c1667f5d964c6fa9e0903afc89c6c
Nico Burns <nico@nicoburns.com>
2022-12-19 02:11:22 +0000

    Import additional flexbox gap tests from yoga (ba27f9d1ecfa7518019845b84b035d3d4a2a6658)

---
+++ tests/generated/gap_column_gap_determines_parent_width.rs
@@ -0,0 +1,63 @@
+#[test]
+fn gap_column_gap_determines_parent_width() {
+    #[allow(unused_imports)]
+    use taffy::prelude::*;
+    let mut taffy = taffy::Taffy::new();
+    let node0 = taffy
+        .new_with_children(
+            taffy::style::Style {
+                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(10f32), ..Size::auto() },
+                ..Default::default()
+            },
+            &[],
+        )
+        .unwrap();
+    let node1 = taffy
+        .new_with_children(
+            taffy::style::Style {
+                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(20f32), ..Size::auto() },
+                ..Default::default()
+            },
+            &[],
+        )
+        .unwrap();
+    let node2 = taffy
+        .new_with_children(
+            taffy::style::Style {
+                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(30f32), ..Size::auto() },
+                ..Default::default()
+            },
+            &[],
+        )
+        .unwrap();
+    let node = taffy
+        .new_with_children(
+            taffy::style::Style {
+                gap: taffy::geometry::Size { width: taffy::style::LengthPercentage::Points(10f32), ..Size::zero() },
+                size: taffy::geometry::Size { height: taffy::style::Dimension::Points(100f32), ..Size::auto() },
+                ..Default::default()
+            },
+            &[node0, node1, node2],
+        )
+        .unwrap();
+    taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
+    println!("\nComputed tree:");
+    taffy::debug::print_tree(&taffy, node);
+    println!();
+    assert_eq!(taffy.layout(node).unwrap().size.width, 80f32);
+    assert_eq!(taffy.layout(node).unwrap().size.height, 100f32);
+    assert_eq!(taffy.layout(node).unwrap().location.x, 0f32);
+    assert_eq!(taffy.layout(node).unwrap().location.y, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().size.width, 10f32);
+    assert_eq!(taffy.layout(node0).unwrap().size.height, 100f32);
+    assert_eq!(taffy.layout(node0).unwrap().location.x, 0f32);
+    assert_eq!(taffy.layout(node0).unwrap().location.y, 0f32);
+    assert_eq!(taffy.layout(node1).unwrap().size.width, 20f32);
+    assert_eq!(taffy.layout(node1).unwrap().size.height, 100f32);
+    assert_eq!(taffy.layout(node1).unwrap().location.x, 20f32);
+    assert_eq!(taffy.layout(node1).unwrap().location.y, 0f32);
+    assert_eq!(taffy.layout(node2).unwrap().size.width, 30f32);
+    assert_eq!(taffy.layout(node2).unwrap().size.height, 100f32);
+    assert_eq!(taffy.layout(node2).unwrap().location.x, 50f32);
+    assert_eq!(taffy.layout(node2).unwrap().location.y, 0f32);
+}
