1891be3ad05d6d93f0c0f9d222ba03d5ca7b5421
Nico Burns <nico@nicoburns.com>
2022-12-26 17:09:46 +0000

    Further simplify grid_percent_simple test

--- benches/generated/grid_percent_simple.rs
+++ benches/generated/grid_percent_simple.rs
@@ -2,7 +2,7 @@ pub fn compute() {
     #[allow(unused_imports)]
     use taffy::prelude::*;
     let mut taffy = taffy::Taffy::new();
-    let node00 = taffy
+    let node0 = taffy
         .new_leaf(taffy::style::Style {
             size: taffy::geometry::Size { width: taffy::style::Dimension::Percent(0.45f32), height: auto() },
             margin: taffy::geometry::Rect {
@@ -20,16 +20,11 @@ pub fn compute() {
             ..Default::default()
         })
         .unwrap();
-    let node0 = taffy
+    let node = taffy
         .new_with_children(
             taffy::style::Style {
                 display: taffy::style::Display::Grid,
-                margin: taffy::geometry::Rect {
-                    left: taffy::style::LengthPercentageAuto::Points(5f32),
-                    right: taffy::style::LengthPercentageAuto::Points(5f32),
-                    top: taffy::style::LengthPercentageAuto::Points(5f32),
-                    bottom: taffy::style::LengthPercentageAuto::Points(5f32),
-                },
+                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(200f32), height: auto() },
                 padding: taffy::geometry::Rect {
                     left: taffy::style::LengthPercentage::Points(3f32),
                     right: taffy::style::LengthPercentage::Points(3f32),
@@ -38,22 +33,7 @@ pub fn compute() {
                 },
                 ..Default::default()
             },
-            &[node00],
-        )
-        .unwrap();
-    let node1 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node = taffy
-        .new_with_children(
-            taffy::style::Style {
-                display: taffy::style::Display::Grid,
-                grid_template_rows: vec![flex(1f32), flex(4f32)],
-                size: taffy::geometry::Size {
-                    width: taffy::style::Dimension::Points(200f32),
-                    height: taffy::style::Dimension::Points(200f32),
-                },
-                ..Default::default()
-            },
-            &[node0, node1],
+            &[node0],
         )
         .unwrap();
     taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
--- tests/generated/grid_percent_simple.rs
+++ tests/generated/grid_percent_simple.rs
@@ -4,7 +4,7 @@ fn grid_percent_simple() {
     #[allow(unused_imports)]
     use taffy::{layout::Layout, prelude::*};
     let mut taffy = taffy::Taffy::new();
-    let node00 = taffy
+    let node0 = taffy
         .new_leaf(taffy::style::Style {
             size: taffy::geometry::Size { width: taffy::style::Dimension::Percent(0.45f32), height: auto() },
             margin: taffy::geometry::Rect {
@@ -22,16 +22,11 @@ fn grid_percent_simple() {
             ..Default::default()
         })
         .unwrap();
-    let node0 = taffy
+    let node = taffy
         .new_with_children(
             taffy::style::Style {
                 display: taffy::style::Display::Grid,
-                margin: taffy::geometry::Rect {
-                    left: taffy::style::LengthPercentageAuto::Points(5f32),
-                    right: taffy::style::LengthPercentageAuto::Points(5f32),
-                    top: taffy::style::LengthPercentageAuto::Points(5f32),
-                    bottom: taffy::style::LengthPercentageAuto::Points(5f32),
-                },
+                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(200f32), height: auto() },
                 padding: taffy::geometry::Rect {
                     left: taffy::style::LengthPercentage::Points(3f32),
                     right: taffy::style::LengthPercentage::Points(3f32),
@@ -40,22 +35,7 @@ fn grid_percent_simple() {
                 },
                 ..Default::default()
             },
-            &[node00],
-        )
-        .unwrap();
-    let node1 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node = taffy
-        .new_with_children(
-            taffy::style::Style {
-                display: taffy::style::Display::Grid,
-                grid_template_rows: vec![flex(1f32), flex(4f32)],
-                size: taffy::geometry::Size {
-                    width: taffy::style::Dimension::Points(200f32),
-                    height: taffy::style::Dimension::Points(200f32),
-                },
-                ..Default::default()
-            },
-            &[node0, node1],
+            &[node0],
         )
         .unwrap();
     taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
@@ -64,22 +44,12 @@ fn grid_percent_simple() {
     println!();
     let Layout { size, location, .. } = taffy.layout(node).unwrap();
     assert_eq!(size.width, 200f32, "width of node {:?}. Expected {}. Actual {}", node.data(), 200f32, size.width);
-    assert_eq!(size.height, 200f32, "height of node {:?}. Expected {}. Actual {}", node.data(), 200f32, size.height);
+    assert_eq!(size.height, 31f32, "height of node {:?}. Expected {}. Actual {}", node.data(), 31f32, size.height);
     assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node.data(), 0f32, location.x);
     assert_eq!(location.y, 0f32, "y of node {:?}. Expected {}. Actual {}", node.data(), 0f32, location.y);
     let Layout { size, location, .. } = taffy.layout(node0).unwrap();
-    assert_eq!(size.width, 190f32, "width of node {:?}. Expected {}. Actual {}", node0.data(), 190f32, size.width);
-    assert_eq!(size.height, 30f32, "height of node {:?}. Expected {}. Actual {}", node0.data(), 30f32, size.height);
-    assert_eq!(location.x, 5f32, "x of node {:?}. Expected {}. Actual {}", node0.data(), 5f32, location.x);
-    assert_eq!(location.y, 5f32, "y of node {:?}. Expected {}. Actual {}", node0.data(), 5f32, location.y);
-    let Layout { size, location, .. } = taffy.layout(node00).unwrap();
-    assert_eq!(size.width, 83f32, "width of node {:?}. Expected {}. Actual {}", node00.data(), 83f32, size.width);
-    assert_eq!(size.height, 6f32, "height of node {:?}. Expected {}. Actual {}", node00.data(), 6f32, size.height);
-    assert_eq!(location.x, 12f32, "x of node {:?}. Expected {}. Actual {}", node00.data(), 12f32, location.x);
-    assert_eq!(location.y, 12f32, "y of node {:?}. Expected {}. Actual {}", node00.data(), 12f32, location.y);
-    let Layout { size, location, .. } = taffy.layout(node1).unwrap();
-    assert_eq!(size.width, 200f32, "width of node {:?}. Expected {}. Actual {}", node1.data(), 200f32, size.width);
-    assert_eq!(size.height, 160f32, "height of node {:?}. Expected {}. Actual {}", node1.data(), 160f32, size.height);
-    assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node1.data(), 0f32, location.x);
-    assert_eq!(location.y, 40f32, "y of node {:?}. Expected {}. Actual {}", node1.data(), 40f32, location.y);
+    assert_eq!(size.width, 87f32, "width of node {:?}. Expected {}. Actual {}", node0.data(), 87f32, size.width);
+    assert_eq!(size.height, 6f32, "height of node {:?}. Expected {}. Actual {}", node0.data(), 6f32, size.height);
+    assert_eq!(location.x, 13f32, "x of node {:?}. Expected {}. Actual {}", node0.data(), 13f32, location.x);
+    assert_eq!(location.y, 13f32, "y of node {:?}. Expected {}. Actual {}", node0.data(), 13f32, location.y);
 }
