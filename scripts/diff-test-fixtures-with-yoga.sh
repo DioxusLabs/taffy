0378c38227ed417fdf9b06242947f27054b51f84
Nico Burns <nico@nicoburns.com>
2023-01-05 13:41:10 +0000

    Fix diff script for column_row_gap test

--- scripts/diff-test-fixtures-with-yoga.sh
+++ scripts/diff-test-fixtures-with-yoga.sh
@@ -11,7 +11,7 @@ if [ ! -d "$YOGA_FIXTURE_DIR" ]; then
   exit 1;
 fi
 
-YOGA_TEST_LIST=$(rg '<div id="([a-z_]*)".*' --replace '$1' -o --no-line-number --no-filename $YOGA_FIXTURE_DIR | grep -v '^$' | sed -E 's/^(row|column)_gap/gap_\1_gap/' | sort)
+YOGA_TEST_LIST=$(rg '<div id="([a-z_]*)".*' --replace '$1' -o --no-line-number --no-filename $YOGA_FIXTURE_DIR | grep -v '^$' | sed -E 's/^(column_row|row|column)_gap/gap_\1_gap/' | sort)
 
 TAFFY_FIXTURE_DIR=$(SELF=$(dirname "$0") && bash -c "cd \"$SELF\" && cd ../test_fixtures && pwd")
 TAFFY_TEST_LIST=$(cd $TAFFY_FIXTURE_DIR && fd -x echo {.} | grep -v '^grid' | sed -E 's/^x(.*)/\1--disabled/' | sort)
