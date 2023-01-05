eccb48dd8b1d324c9a0869154e36750bb246a854
Nico Burns <nico@nicoburns.com>
2023-01-05 00:34:35 +0000

    Modify diff script to display disabled tests next to enabled ones

--- scripts/diff-test-fixtures-with-yoga.sh
+++ scripts/diff-test-fixtures-with-yoga.sh
@@ -5,7 +5,7 @@ YOGA_FIXTURE_DIR=$1
 YOGA_TEST_LIST=$(rg '<div id="([a-z_]*)".*' --replace '$1' -o --no-line-number --no-filename $1 | grep -v '^$' | sed -E 's/^(row|column)_gap/gap_\1_gap/' | sort)
 
 TAFFY_FIXTURE_DIR=$(SELF=$(dirname "$0") && bash -c "cd \"$SELF\" && cd ../test_fixtures && pwd")
-TAFFY_TEST_LIST=$(cd $TAFFY_FIXTURE_DIR && fd -x echo {.} | grep -v '^grid' | sort)
+TAFFY_TEST_LIST=$(cd $TAFFY_FIXTURE_DIR && fd -x echo {.} | grep -v '^grid' | sed -E 's/^x(.*)/\1--disabled/' | sort)
 
 # DIFF=$(diff <(echo "$YOGA_TEST_LIST") <(echo "$TAFFY_TEST_LIST"))
 # echo "$DIFF"
