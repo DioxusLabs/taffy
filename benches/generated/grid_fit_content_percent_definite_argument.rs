b557f6f22a5167dac73c88ce13edad9421485472
Nico Burns <nico@nicoburns.com>
2023-01-22 16:05:24 +0000

    Simplify fit-content percent definite test

--- benches/generated/grid_fit_content_percent_definite_argument.rs
+++ benches/generated/grid_fit_content_percent_definite_argument.rs
@@ -2,8 +2,7 @@ pub fn compute() {
     #[allow(unused_imports)]
     use taffy::prelude::*;
     let mut taffy = taffy::Taffy::new();
-    let node0 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node1 = taffy
+    let node0 = taffy
         .new_leaf_with_measure(
             taffy::style::Style { ..Default::default() },
             taffy::node::MeasureFunc::Raw(|known_dimensions, available_space| {
@@ -18,23 +17,16 @@ pub fn compute() {
             }),
         )
         .unwrap();
-    let node2 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node3 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node4 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node5 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node6 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node7 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
-    let node8 = taffy.new_leaf(taffy::style::Style { ..Default::default() }).unwrap();
     let node = taffy
         .new_with_children(
             taffy::style::Style {
                 display: taffy::style::Display::Grid,
-                grid_template_rows: vec![points(40f32), points(40f32), points(40f32)],
-                grid_template_columns: vec![points(40f32), fit_content(percent(0.1f32)), points(40f32)],
-                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(300f32), height: auto() },
+                grid_template_rows: vec![points(40f32)],
+                grid_template_columns: vec![fit_content(percent(0.5f32))],
+                size: taffy::geometry::Size { width: taffy::style::Dimension::Points(60f32), height: auto() },
                 ..Default::default()
             },
-            &[node0, node1, node2, node3, node4, node5, node6, node7, node8],
+            &[node0],
         )
         .unwrap();
     taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
