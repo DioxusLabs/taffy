f6e4202704277c98a189539333ee4254c63a1f90
Nico Burns <nico@nicoburns.com>
2023-03-05 16:29:17 +0000

    Add failing tests for padding flooring node size

---
+++ tests/generated/padding_overrides_min_size.rs
@@ -0,0 +1,31 @@
+#[test]
+fn padding_overrides_min_size() {
+    use slotmap::Key;
+    #[allow(unused_imports)]
+    use taffy::{layout::Layout, prelude::*};
+    let mut taffy = taffy::Taffy::new();
+    let node = taffy
+        .new_leaf(taffy::style::Style {
+            min_size: taffy::geometry::Size {
+                width: taffy::style::Dimension::Points(0f32),
+                height: taffy::style::Dimension::Points(0f32),
+            },
+            padding: taffy::geometry::Rect {
+                left: taffy::style::LengthPercentage::Points(10f32),
+                right: taffy::style::LengthPercentage::Points(10f32),
+                top: taffy::style::LengthPercentage::Points(10f32),
+                bottom: taffy::style::LengthPercentage::Points(10f32),
+            },
+            ..Default::default()
+        })
+        .unwrap();
+    taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
+    println!("\nComputed tree:");
+    taffy::debug::print_tree(&taffy, node);
+    println!();
+    let Layout { size, location, .. } = taffy.layout(node).unwrap();
+    assert_eq!(size.width, 20f32, "width of node {:?}. Expected {}. Actual {}", node.data(), 20f32, size.width);
+    assert_eq!(size.height, 20f32, "height of node {:?}. Expected {}. Actual {}", node.data(), 20f32, size.height);
+    assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node.data(), 0f32, location.x);
+    assert_eq!(location.y, 0f32, "y of node {:?}. Expected {}. Actual {}", node.data(), 0f32, location.y);
+}
