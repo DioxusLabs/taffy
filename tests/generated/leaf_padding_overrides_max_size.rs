fbf950e8ade5f0ad037d9770e514bfbf82778598
Nico Burns <nico@nicoburns.com>
2023-03-05 21:57:29 +0000

    Add seperate leaf and flexbox tests for padding/border flooring node size

--- tests/generated/leaf_padding_overrides_max_size.rs
+++ tests/generated/leaf_padding_overrides_max_size.rs
@@ -1,10 +1,10 @@
 #[test]
-fn padding_overrides_max_size() {
+fn leaf_padding_overrides_max_size() {
     use slotmap::Key;
     #[allow(unused_imports)]
     use taffy::{layout::Layout, prelude::*};
     let mut taffy = taffy::Taffy::new();
-    let node0 = taffy
+    let node = taffy
         .new_leaf(taffy::style::Style {
             max_size: taffy::geometry::Size {
                 width: taffy::style::Dimension::Points(5f32),
@@ -19,7 +19,6 @@ fn padding_overrides_max_size() {
             ..Default::default()
         })
         .unwrap();
-    let node = taffy.new_with_children(taffy::style::Style { ..Default::default() }, &[node0]).unwrap();
     taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
     println!("\nComputed tree:");
     taffy::debug::print_tree(&taffy, node);
@@ -29,9 +28,4 @@ fn padding_overrides_max_size() {
     assert_eq!(size.height, 20f32, "height of node {:?}. Expected {}. Actual {}", node.data(), 20f32, size.height);
     assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node.data(), 0f32, location.x);
     assert_eq!(location.y, 0f32, "y of node {:?}. Expected {}. Actual {}", node.data(), 0f32, location.y);
-    let Layout { size, location, .. } = taffy.layout(node0).unwrap();
-    assert_eq!(size.width, 20f32, "width of node {:?}. Expected {}. Actual {}", node0.data(), 20f32, size.width);
-    assert_eq!(size.height, 20f32, "height of node {:?}. Expected {}. Actual {}", node0.data(), 20f32, size.height);
-    assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node0.data(), 0f32, location.x);
-    assert_eq!(location.y, 0f32, "y of node {:?}. Expected {}. Actual {}", node0.data(), 0f32, location.y);
 }
