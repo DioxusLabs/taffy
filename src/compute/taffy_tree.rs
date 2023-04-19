c0acdeeb469e770f7fb3177a8de24c02667778fd
Nico Burns <nico@nicoburns.com>
2023-04-19 13:11:53 +0100

    Clear cache when applying hidden layout to a node

--- src/compute/taffy_tree.rs
+++ src/compute/taffy_tree.rs
@@ -1,6 +1,6 @@
 //! Computation specific for the default `Taffy` tree implementation
 
-use crate::compute::{leaf, HiddenAlgorithm, LayoutAlgorithm};
+use crate::compute::{leaf, LayoutAlgorithm};
 use crate::geometry::{Point, Size};
 use crate::style::{AvailableSpace, Display};
 use crate::tree::{Layout, LayoutTree, NodeId, RunMode, SizeAndBaselines, SizingMode, Taffy, TaffyError};
@@ -143,15 +143,10 @@ fn compute_node_layout(
 
     let display_mode = tree.nodes[node_key].style.display;
     let computed_size_and_baselines = match (display_mode, has_children) {
-        (Display::None, _) => perform_computations::<HiddenAlgorithm>(
-            tree,
-            node,
-            known_dimensions,
-            parent_size,
-            available_space,
-            run_mode,
-            sizing_mode,
-        ),
+        (Display::None, _) => {
+            perform_taffy_tree_hidden_layout(tree, node);
+            SizeAndBaselines { size: Size::ZERO, first_baselines: Point::NONE }
+        }
         #[cfg(feature = "flexbox")]
         (Display::Flex, true) => perform_computations::<FlexboxAlgorithm>(
             tree,
@@ -202,6 +197,24 @@ fn compute_node_layout(
     NODE_LOGGER.pop_node();
 
     computed_size_and_baselines
+}
+
+/// Creates a layout for this node and its children, recursively.
+/// Each hidden node has zero size and is placed at the origin
+fn perform_taffy_tree_hidden_layout(tree: &mut Taffy, node: NodeId) {
+    /// Recursive function to apply hidden layout to all descendents
+    fn perform_hidden_layout_inner(tree: &mut Taffy, node: NodeId, order: u32) {
+        let node_key = node.into();
+        *tree.layout_mut(node) = Layout::with_order(order);
+        tree.nodes[node_key].cache.clear();
+        for order in 0..tree.children[node_key].len() {
+            perform_hidden_layout_inner(tree, tree.child(node, order), order as _);
+        }
+    }
+
+    for order in 0..tree.children[node.into()].len() {
+        perform_hidden_layout_inner(tree, tree.child(node, order), order as _);
+    }
 }
 
 /// Rounds the calculated [`Layout`] to exact pixel values
