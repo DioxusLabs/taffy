ef6f63f455389db4157ec859d279ab6b0abce04c
Nico Burns <nico@nicoburns.com>
2023-04-24 11:01:50 +0100

    Move git check to bash script file

---
+++ scripts/format-fixtures/check-if-git-dirty.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+  echo "Fixtures formatted correctly"
+  exit(0)
+else 
+  # Uncommitted changes
+  echo "Test fixtures are not formatted correctly."
+  echo "Please run 'cargo format-fixtures'"
+  exit(1)
+fi
