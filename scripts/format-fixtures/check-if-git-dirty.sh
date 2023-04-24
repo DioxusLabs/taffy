1834e688334f3207ce237f42c158d335ffb45a11
Nico Burns <nico@nicoburns.com>
2023-04-24 11:04:33 +0100

    Fix bash script

--- scripts/format-fixtures/check-if-git-dirty.sh
+++ scripts/format-fixtures/check-if-git-dirty.sh
@@ -3,10 +3,10 @@
 if [ -z "$(git status --porcelain)" ]; then 
   # Working directory clean
   echo "Fixtures formatted correctly"
-  exit(0)
+  exit 0
 else 
   # Uncommitted changes
   echo "Test fixtures are not formatted correctly."
   echo "Please run 'cargo format-fixtures'"
-  exit(1)
+  exit 1
 fi
