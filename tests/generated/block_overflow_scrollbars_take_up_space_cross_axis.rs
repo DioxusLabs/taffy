da5351ed85b8fb0b202948c35ffdf6bcd82aff85
Nico Burns <nico@nicoburns.com>
2023-05-04 23:53:30 +0100

    Add tests for scrollbars taking up space in block layout

---
+++ tests/generated/block_overflow_scrollbars_take_up_space_cross_axis.rs
@@ -0,0 +1,50 @@
+#[test]
+fn block_overflow_scrollbars_take_up_space_cross_axis() {
+    #[allow(unused_imports)]
+    use taffy::{prelude::*, tree::Layout};
+    let mut taffy = taffy::Taffy::new();
+    let node0 = taffy
+        .new_leaf(taffy::style::Style {
+            position: taffy::style::Position::Absolute,
+            inset: taffy::geometry::Rect {
+                left: taffy::style::LengthPercentageAuto::Length(0f32),
+                right: taffy::style::LengthPercentageAuto::Length(0f32),
+                top: taffy::style::LengthPercentageAuto::Length(0f32),
+                bottom: taffy::style::LengthPercentageAuto::Length(0f32),
+            },
+            ..Default::default()
+        })
+        .unwrap();
+    let node = taffy
+        .new_with_children(
+            taffy::style::Style {
+                display: taffy::style::Display::Block,
+                overflow: taffy::geometry::Point {
+                    x: taffy::style::Overflow::Visible,
+                    y: taffy::style::Overflow::Scroll,
+                },
+                scrollbar_width: 15f32,
+                size: taffy::geometry::Size {
+                    width: taffy::style::Dimension::Length(50f32),
+                    height: taffy::style::Dimension::Length(50f32),
+                },
+                ..Default::default()
+            },
+            &[node0],
+        )
+        .unwrap();
+    taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
+    println!("\nComputed tree:");
+    taffy::util::print_tree(&taffy, node);
+    println!();
+    let Layout { size, location, .. } = taffy.layout(node).unwrap();
+    assert_eq!(size.width, 50f32, "width of node {:?}. Expected {}. Actual {}", node, 50f32, size.width);
+    assert_eq!(size.height, 50f32, "height of node {:?}. Expected {}. Actual {}", node, 50f32, size.height);
+    assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node, 0f32, location.x);
+    assert_eq!(location.y, 0f32, "y of node {:?}. Expected {}. Actual {}", node, 0f32, location.y);
+    let Layout { size, location, .. } = taffy.layout(node0).unwrap();
+    assert_eq!(size.width, 35f32, "width of node {:?}. Expected {}. Actual {}", node0, 35f32, size.width);
+    assert_eq!(size.height, 50f32, "height of node {:?}. Expected {}. Actual {}", node0, 50f32, size.height);
+    assert_eq!(location.x, 0f32, "x of node {:?}. Expected {}. Actual {}", node0, 0f32, location.x);
+    assert_eq!(location.y, 0f32, "y of node {:?}. Expected {}. Actual {}", node0, 0f32, location.y);
+}
