dde4431fa054967db95aa14f4c98c9ddfb607b63
Nico Burns <nico@nicoburns.com>
2023-10-05 15:57:09 +0100

    Regenerate tests after rebase

--- tests/generated/flex/justify_content_column_space_evenly_negative_space.rs
+++ tests/generated/flex/justify_content_column_space_evenly_negative_space.rs
@@ -1,8 +1,8 @@
 #[test]
 fn justify_content_column_space_evenly_negative_space() {
     #[allow(unused_imports)]
-    use taffy::{prelude::*, tree::Layout};
-    let mut taffy = taffy::Taffy::new();
+    use taffy::{prelude::*, tree::Layout, Taffy};
+    let mut taffy: Taffy<crate::TextMeasure> = Taffy::new();
     let node00 = taffy
         .new_leaf(taffy::style::Style {
             flex_shrink: 0f32,
@@ -66,9 +66,9 @@ fn justify_content_column_space_evenly_negative_space() {
             &[node0],
         )
         .unwrap();
-    taffy.compute_layout(node, taffy::geometry::Size::MAX_CONTENT).unwrap();
+    taffy.compute_layout_with_measure(node, taffy::geometry::Size::MAX_CONTENT, crate::test_measure_function).unwrap();
     println!("\nComputed tree:");
-    taffy::util::print_tree(&taffy, node);
+    taffy.print_tree(node);
     println!();
     let Layout { size, location, .. } = taffy.layout(node).unwrap();
     assert_eq!(size.width, 320f32, "width of node {:?}. Expected {}. Actual {}", node, 320f32, size.width);
