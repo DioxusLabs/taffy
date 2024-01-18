2b3aa8cc910db2d66e45e7f693875e59ba73a5f3
Nico Burns <nico@nicoburns.com>
2024-01-18 14:48:53 +1300

    Add overflow to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,7 +4,7 @@ pub use crate::{
     geometry::{Line, Rect, Size},
     style::{
         AlignContent, AlignItems, AlignSelf, AvailableSpace, Dimension, Display, JustifyContent, JustifyItems,
-        JustifySelf, LengthPercentage, LengthPercentageAuto, Position, Style,
+        JustifySelf, LengthPercentage, LengthPercentageAuto, Overflow, Position, Style,
     },
     style_helpers::{
         auto, fit_content, length, max_content, min_content, percent, zero, FromFlex, FromLength, FromPercent,
