824a3fd742f3dcf6a97c84a360efbd1b7de6f884
Nico Burns <nico@nicoburns.com>
2024-07-12 11:14:21 +1200

    Update examples to use new traits

--- examples/custom_tree_owned_unsafe.rs
+++ examples/custom_tree_owned_unsafe.rs
@@ -130,7 +130,12 @@ impl TraversePartialTree for StatelessLayoutTree {
 impl TraverseTree for StatelessLayoutTree {}
 
 impl LayoutPartialTree for StatelessLayoutTree {
-    fn get_style(&self, node_id: NodeId) -> &Style {
+    type CoreContainerStyle<'a> = &'a Style where
+        Self: 'a;
+
+    type CacheMut<'b> = &'b mut Cache where Self: 'b;
+
+    fn get_core_container_style(&self, node_id: NodeId) -> Self::CoreContainerStyle<'_> {
         unsafe { &node_from_id(node_id).style }
     }
 
@@ -163,6 +168,38 @@ impl LayoutPartialTree for StatelessLayoutTree {
                 }),
             }
         })
+    }
+}
+
+impl taffy::LayoutFlexboxContainer for StatelessLayoutTree {
+    type ContainerStyle<'a> = &'a Style where
+        Self: 'a;
+
+    type ItemStyle<'a> = &'a Style where
+        Self: 'a;
+
+    fn get_flexbox_container_style(&self, node_id: NodeId) -> Self::ContainerStyle<'_> {
+        unsafe { &node_from_id(node_id).style }
+    }
+
+    fn get_flexbox_child_style(&self, child_node_id: NodeId) -> Self::ItemStyle<'_> {
+        unsafe { &node_from_id(child_node_id).style }
+    }
+}
+
+impl taffy::LayoutGridContainer for StatelessLayoutTree {
+    type ContainerStyle<'a> = &'a Style where
+        Self: 'a;
+
+    type ItemStyle<'a> = &'a Style where
+        Self: 'a;
+
+    fn get_grid_container_style(&self, node_id: NodeId) -> Self::ContainerStyle<'_> {
+        unsafe { &node_from_id(node_id).style }
+    }
+
+    fn get_grid_child_style(&self, child_node_id: NodeId) -> Self::ItemStyle<'_> {
+        unsafe { &node_from_id(child_node_id).style }
     }
 }
 
