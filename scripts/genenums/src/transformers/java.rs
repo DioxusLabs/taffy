798a82ab3aba7c03441bb76e628cb0292c0998b0
Arby Djabaev <contact@arby.be>
2024-09-03 01:37:57 +0200

    genenums: rustfmt stuff

--- scripts/genenums/src/transformers/java.rs
+++ scripts/genenums/src/transformers/java.rs
@@ -4,7 +4,8 @@ use std::io::Write;
 use std::path::Path;
 
 pub(crate) fn create_java_tranformer(name: &str, values: &[&str], default: bool) {
-    let mut file_content: String = "use crate::traits::FromJavaEnum;\n/// FromJavaEnum implementations for Enums that need it\n".to_string();
+    let mut file_content: String =
+        "use crate::traits::FromJavaEnum;\n/// FromJavaEnum implementations for Enums that need it\n".to_string();
 
     if Path::new("./bindings/java/src/enums.rs").exists() {
         file_content = fs::read_to_string(Path::new("./bindings/java/src/enums.rs")).unwrap()
@@ -29,7 +30,8 @@ pub(crate) fn create_java_tranformer(name: &str, values: &[&str], default: bool)
         .as_str(),
     );
 
-    file_content = format!("{file_content}
+    file_content = format!(
+        "{file_content}
 impl FromJavaEnum<{name}> for {name} {{
     const JAVA_CLASS: &'static str = \"Lcom/dioxuslabs/taffy/enums/{name};\";
 
