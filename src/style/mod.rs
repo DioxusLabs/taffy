eed38fe0f74aeca94ce2e73b16f4111008079928
Nico Burns <nico@nicoburns.com>
2025-10-13 16:31:41 +0100

    Add static and fixed position styles
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- src/style/mod.rs
+++ src/style/mod.rs
@@ -254,6 +254,8 @@ impl Default for BoxGenerationMode {
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Position {
+    /// Offets are not applied
+    Static,
     /// The offset is computed relative to the final position given by the layout algorithm.
     /// Offsets do not affect the position of any other items; they are effectively a correction factor applied at the end.
     Relative,
@@ -263,11 +265,33 @@ pub enum Position {
     ///
     /// WARNING: to opt-out of layouting entirely, you must use [`Display::None`] instead on your [`Style`] object.
     Absolute,
+    /// The offset is computed relative to the viewport or transform boundary
+    Fixed,
 }
 
 impl Default for Position {
     fn default() -> Self {
         Self::Relative
+    }
+}
+
+impl Position {
+    /// Whether the element has a non-static position
+    #[inline(always)]
+    pub fn is_positioned(self) -> bool {
+        !matches!(self, Self::Static)
+    }
+
+    /// Whether the element is positioned out-of-flow (absolute or fixed position)
+    #[inline(always)]
+    pub fn is_out_of_flow(self) -> bool {
+        matches!(self, Self::Absolute | Self::Fixed)
+    }
+
+    /// Whether the element is positioned in-flow (NOT absolute or fixed position)
+    #[inline(always)]
+    pub fn is_in_flow(self) -> bool {
+        !self.is_out_of_flow()
     }
 }
 
