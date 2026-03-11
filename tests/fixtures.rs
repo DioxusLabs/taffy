2716c1e362bf58ee6579c3e5eedd02864e139ebb
Nico Burns <nico@nicoburns.com>
2026-03-10 17:23:53 -0700

    Delete old gentests
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- tests/fixtures.rs
+++
@@ -1,6 +0,0 @@
-// this declaration is necessary to "mount" the generated code where cargo can see it
-// this allows us to both keep code generation scoped to a singe directory for fs events
-// and to keep each test in a separate file
-// mod generated;
-
-// pub use taffy_test_helpers::{new_test_tree, test_measure_function, TestNodeContext, WritingMode};
