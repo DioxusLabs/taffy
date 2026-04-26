d803b72f958991514d47fae7f877198e126402d1
Nico Burns <nico@nicoburns.com>
2026-04-26 15:44:24 +0100

    Comine integration tests into one binary (#951)
    Signed-off-by: Nico Burns <nico@nicoburns.com>

---
+++ tests/hand_written.rs
@@ -0,0 +1,10 @@
+mod hand_written {
+    mod border_and_padding;
+    mod caching;
+    mod measure;
+    mod min_max_overrides;
+    mod relayout;
+    mod root_constraints;
+    mod rounding;
+    mod serde;
+}
