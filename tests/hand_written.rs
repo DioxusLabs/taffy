80e49e1f20312c6d49046df21a49243847a66b1e
nico.burns <nico@nicoburns.com>
2026-07-29 04:01:29 +0000

    Convert issue #999 regression tests to generated tests

--- tests/hand_written.rs
+++ tests/hand_written.rs
@@ -4,7 +4,6 @@ mod hand_written {
     mod block_replaced;
     mod border_and_padding;
     mod caching;
-    mod flex_definiteness;
     mod floats;
     mod measure;
     mod min_max_overrides;
--- tests/xml/mod.rs
+++ tests/xml/mod.rs
@@ -16762,6 +16762,46 @@ mod flex {
     }
 
     #[test]
+    fn wrap_column_definite_main_size_percent_basis__border_box_ltr() {
+        crate::run_xml_test("flex", "wrap_column_definite_main_size_percent_basis__border_box_ltr");
+    }
+
+    #[test]
+    fn wrap_column_definite_main_size_percent_basis__content_box_ltr() {
+        crate::run_xml_test("flex", "wrap_column_definite_main_size_percent_basis__content_box_ltr");
+    }
+
+    #[test]
+    fn wrap_column_definite_main_size_percent_basis__border_box_rtl() {
+        crate::run_xml_test("flex", "wrap_column_definite_main_size_percent_basis__border_box_rtl");
+    }
+
+    #[test]
+    fn wrap_column_definite_main_size_percent_basis__content_box_rtl() {
+        crate::run_xml_test("flex", "wrap_column_definite_main_size_percent_basis__content_box_rtl");
+    }
+
+    #[test]
+    fn wrap_column_indefinite_main_size_percent_basis__border_box_ltr() {
+        crate::run_xml_test("flex", "wrap_column_indefinite_main_size_percent_basis__border_box_ltr");
+    }
+
+    #[test]
+    fn wrap_column_indefinite_main_size_percent_basis__content_box_ltr() {
+        crate::run_xml_test("flex", "wrap_column_indefinite_main_size_percent_basis__content_box_ltr");
+    }
+
+    #[test]
+    fn wrap_column_indefinite_main_size_percent_basis__border_box_rtl() {
+        crate::run_xml_test("flex", "wrap_column_indefinite_main_size_percent_basis__border_box_rtl");
+    }
+
+    #[test]
+    fn wrap_column_indefinite_main_size_percent_basis__content_box_rtl() {
+        crate::run_xml_test("flex", "wrap_column_indefinite_main_size_percent_basis__content_box_rtl");
+    }
+
+    #[test]
     fn wrap_grandchild__border_box_ltr() {
         crate::run_xml_test("flex", "wrap_grandchild__border_box_ltr");
     }
