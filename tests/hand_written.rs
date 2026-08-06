8f45dd6853d6dccb1d892a1f4de33132edf8df64
Nico Burns <nico@nicoburns.com>
2026-08-06 15:53:12 +0100

    Block: compute clearance from the hypothetical position of the cleared element (#1042)
    * Block: compute clearance from the hypothetical position of the cleared element
    
    * Convert clearance tests to generated tests (clear no-op, negative clearance)

--- tests/hand_written.rs
+++ tests/hand_written.rs
@@ -3,8 +3,6 @@ mod hand_written {
     mod block_replaced;
     mod border_and_padding;
     mod caching;
-    #[cfg(feature = "float_layout")]
-    mod float_clearance;
     mod floats;
     mod measure;
     mod min_max_overrides;
