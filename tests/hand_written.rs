4d3b328b97a44034141c305333eae08eb09b3f78
Nico Burns <nico@nicoburns.com>
2026-08-06 01:18:31 +0100

    Float: treat clear with no relevant floats as a no-op (#1041)

--- tests/hand_written.rs
+++ tests/hand_written.rs
@@ -3,6 +3,8 @@ mod hand_written {
     mod block_replaced;
     mod border_and_padding;
     mod caching;
+    #[cfg(feature = "float_layout")]
+    mod float_clearance;
     mod floats;
     mod measure;
     mod min_max_overrides;
