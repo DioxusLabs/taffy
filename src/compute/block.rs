e24d864e83908c539b67e440a7161fb4220e7b2e
Nico Burns <nico@nicoburns.com>
2026-08-13 21:14:36 +0000

    Fix rebase: add known_dimensions_are_definite to LayoutInput

--- src/compute/block.rs
+++ src/compute/block.rs
@@ -1159,6 +1159,7 @@ fn perform_final_layout_on_in_flow_children(
                                         sizing_mode: SizingMode::InherentSize,
                                         axis: RequestedAxis::Both,
                                         known_dimensions,
+                                        known_dimensions_are_definite: Size { width: true, height: true },
                                         parent_size,
                                         available_space: available_space
                                             .map_width(|_| AvailableSpace::Definite(stretch_width)),
